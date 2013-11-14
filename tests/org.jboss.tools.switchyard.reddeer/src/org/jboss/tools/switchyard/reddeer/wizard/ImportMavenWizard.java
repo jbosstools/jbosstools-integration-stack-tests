53d3825f2b8367d09d69fe6b2d85515fff90a9ce
Andrej Podhradsky <apodhrad@redhat.com>
2013-11-14 14:43:27 +0100

    JBTIS-125: Create bot tests for all SY quickstarts

---
+++ tests/org.jboss.tools.switchyard.reddeer/src/org/jboss/tools/switchyard/reddeer/wizard/ImportMavenWizard.java
@@ -0,0 +1,73 @@
+package org.jboss.tools.switchyard.reddeer.wizard;
+
+import org.jboss.reddeer.eclipse.jdt.ui.ProjectExplorer;
+import org.jboss.reddeer.eclipse.jface.wizard.ImportWizardDialog;
+import org.jboss.reddeer.swt.condition.JobIsRunning;
+import org.jboss.reddeer.swt.condition.ShellWithTextIsActive;
+import org.jboss.reddeer.swt.condition.WaitCondition;
+import org.jboss.reddeer.swt.exception.SWTLayerException;
+import org.jboss.reddeer.swt.impl.button.CheckBox;
+import org.jboss.reddeer.swt.impl.button.PushButton;
+import org.jboss.reddeer.swt.impl.combo.DefaultCombo;
+import org.jboss.reddeer.swt.impl.menu.ContextMenu;
+import org.jboss.reddeer.swt.impl.shell.DefaultShell;
+import org.jboss.reddeer.swt.wait.AbstractWait;
+import org.jboss.reddeer.swt.wait.TimePeriod;
+import org.jboss.reddeer.swt.wait.WaitUntil;
+import org.jboss.reddeer.swt.wait.WaitWhile;
+
+public class ImportMavenWizard extends ImportWizardDialog {
+
+	public ImportMavenWizard() {
+		super("Maven", "Existing Maven Projects");
+	}
+
+	public void importProject(String path) {
+		open();
+
+		new DefaultShell("Import Maven Projects");
+		new DefaultCombo("Root Directory:").setText(path);
+		new PushButton("Refresh").click();
+		new WaitUntil(new ProjectsIsLoaded(), TimePeriod.VERY_LONG);
+		if (new PushButton("Next >").isEnabled()) {
+			new PushButton("Next >").click();
+		}
+		new PushButton("Finish").click();
+		try {
+			new DefaultShell("Incomplete Maven Goal Execution");
+			new PushButton("OK").click();
+		} catch (SWTLayerException e) {
+			// ok, it means that the warning wasn't displayed
+		}
+		new WaitWhile(new ShellWithTextIsActive("Import Maven Projects"), TimePeriod.LONG);
+		new WaitWhile(new JobIsRunning(), TimePeriod.getCustom(20 * 60 * 1000));
+
+		AbstractWait.sleep(10 * 1000);
+		new WaitWhile(new JobIsRunning(), TimePeriod.LONG);
+
+		ProjectExplorer projectExplorer = new ProjectExplorer();
+		projectExplorer.open();
+		projectExplorer.getProjects().get(0).select();
+		new ContextMenu("Maven", "Update Project...").select();
+		new DefaultShell("Update Maven Project");
+		new CheckBox("Force Update of Snapshots/Releases").toggle(true);
+		new PushButton("OK").click();
+
+		AbstractWait.sleep(10 * 1000);
+		new WaitWhile(new JobIsRunning(), TimePeriod.LONG);
+	}
+
+	protected class ProjectsIsLoaded implements WaitCondition {
+
+		@Override
+		public boolean test() {
+			return new PushButton("Finish").isEnabled() || new PushButton("Next >").isEnabled();
+		}
+
+		@Override
+		public String description() {
+			return "The project is still not loaded.";
+		}
+
+	}
+}
