96d6e36624f801c5c91e682f30a710a17a023407
Andrej Podhradsky <apodhrad@redhat.com>
2013-12-04 09:37:43 +0100

    SwitchYard: added tests into AllTests suite

--- tests/org.jboss.tools.switchyard.ui.bot.test/src/org/jboss/tools/switchyard/ui/bot/test/suite/AllTests.java
+++ tests/org.jboss.tools.switchyard.ui.bot.test/src/org/jboss/tools/switchyard/ui/bot/test/suite/AllTests.java
@@ -3,10 +3,12 @@ package org.jboss.tools.switchyard.ui.bot.test.suite;
 import junit.framework.TestSuite;
 
 import org.jboss.tools.switchyard.ui.bot.test.BPMTest;
+import org.jboss.tools.switchyard.ui.bot.test.BindingsTest;
 import org.jboss.tools.switchyard.ui.bot.test.BottomUpBPELTest;
 import org.jboss.tools.switchyard.ui.bot.test.BottomUpBPMN2Test;
 import org.jboss.tools.switchyard.ui.bot.test.BottomUpCamelTest;
 import org.jboss.tools.switchyard.ui.bot.test.BottomUpEJBTest;
+import org.jboss.tools.switchyard.ui.bot.test.DomainSettingsTest;
 import org.jboss.tools.switchyard.ui.bot.test.DroolsTest;
 import org.jboss.tools.switchyard.ui.bot.test.FileGatewayTest;
 import org.jboss.tools.switchyard.ui.bot.test.ImplementationsPropertiesTest;
@@ -33,7 +35,9 @@ import org.junit.runners.Suite.SuiteClasses;
 	BottomUpBPMN2Test.class,
 	BPMTest.class,
 	DroolsTest.class,
-	ImplementationsPropertiesTest.class
+	ImplementationsPropertiesTest.class,
+	DomainSettingsTest.class,
+	BindingsTest.class
 })
 @RunWith(SwitchyardSuite.class)
 public class AllTests extends TestSuite {
