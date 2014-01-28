995644833e27155e44e96bf3820ad4fb7a77811e
Tomas Schlosser <tschloss@redhat.com>
2014-01-28 15:24:19 +0100

    BRMS: Added WorkingMemoryViewTest

---
+++ tests/org.jboss.tools.drools.reddeer/src/org/jboss/tools/drools/reddeer/view/WorkingMemoryView.java
@@ -0,0 +1,67 @@
+package org.jboss.tools.drools.reddeer.view;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.log4j.Logger;
+import org.jboss.reddeer.swt.api.TreeItem;
+import org.jboss.reddeer.swt.impl.tree.DefaultTree;
+import org.jboss.reddeer.swt.matcher.RegexMatchers;
+import org.jboss.reddeer.workbench.view.impl.WorkbenchView;
+import org.jboss.tools.drools.reddeer.util.ItemLookup;
+
+public class WorkingMemoryView extends WorkbenchView {
+    private static final Pattern OBJECT_PATTERN = Pattern.compile("\\[\\d+\\]= ([^ ]+) +\\(id=\\d+\\)");
+    private static final Pattern ATTRIBUTE_PATTERN = Pattern.compile("(\\w+)= ([^\\(]+).*");
+    private static final Logger LOGGER = Logger.getLogger(WorkingMemoryView.class);
+
+    public WorkingMemoryView() {
+        super("Drools", "Working Memory");
+    }
+
+    public List<String> getObjects() {
+        open();
+        List<String> result = new LinkedList<String>();
+
+        Matcher m;
+        String text;
+        for (TreeItem item : new DefaultTree().getItems()) {
+            text = item.getText();
+            m = OBJECT_PATTERN.matcher(text);
+            if (m.matches()) {
+                result.add(m.group(1));
+            } else {
+                LOGGER.error(String.format("Non matching string: '%s'", text));
+            }
+        }
+
+        return result;
+    }
+
+    public Map<String, String> getObjectAttributes(String objectName) {
+        open();
+        Map<String, String> result = new HashMap<String, String>();
+
+        TreeItem obj = ItemLookup.getItemInTree(new DefaultTree(), new RegexMatchers(".*" + Pattern.quote(objectName) + ".*").getMatchers());
+        if (obj == null) {
+            throw new IllegalArgumentException(String.format("Unable to find '%s'", objectName));
+        }
+        Matcher m;
+        String text;
+        for (TreeItem item : obj.getItems()) {
+            text = item.getText();
+            m = ATTRIBUTE_PATTERN.matcher(text);
+            if (m.matches()) {
+                result.put(m.group(1), m.group(2).trim());
+            } else {
+                LOGGER.error(String.format("Non matching string: '%s'", text));
+            }
+        }
+
+        return result;
+    }
+}
