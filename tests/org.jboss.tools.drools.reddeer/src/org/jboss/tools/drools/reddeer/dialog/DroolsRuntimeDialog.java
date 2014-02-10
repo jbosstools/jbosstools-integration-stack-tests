90b5cbe3d4f848ab468d4455439f1d51b5547db0
Tomas Schlosser <tschloss@redhat.com>
2014-02-10 19:37:01 +0100

    BRMS: Remove Drools plugin dependency

--- tests/org.jboss.tools.drools.reddeer/src/org/jboss/tools/drools/reddeer/dialog/DroolsRuntimeDialog.java
+++ tests/org.jboss.tools.drools.reddeer/src/org/jboss/tools/drools/reddeer/dialog/DroolsRuntimeDialog.java
@@ -1,8 +1,11 @@
 package org.jboss.tools.drools.reddeer.dialog;
 
-import org.drools.eclipse.util.DroolsRuntimeManager;
+import java.lang.reflect.Method;
+
+import org.eclipse.core.runtime.Platform;
 import org.jboss.reddeer.swt.impl.button.PushButton;
 import org.jboss.reddeer.swt.impl.text.LabeledText;
+import org.osgi.framework.wiring.BundleWiring;
 
 public class DroolsRuntimeDialog {
 
@@ -16,7 +19,14 @@ public class DroolsRuntimeDialog {
 
     public void createNewRuntime(String location) {
         // FIXME find a way to do this using SWTBot/RedDeer
-        DroolsRuntimeManager.createDefaultRuntime(location);
+        ClassLoader pluginCl = Platform.getBundle("org.drools.eclipse").adapt(BundleWiring.class).getClassLoader();
+        try {
+            Class<?> clazz = pluginCl.loadClass("org.drools.eclipse.util.DroolsRuntimeManager");
+            Method method = clazz.getMethod("createDefaultRuntime", String.class);
+            method.invoke(null, location);
+        } catch (ReflectiveOperationException ex) {
+            ex.printStackTrace();
+        }
         setLocation(location);
     }
 
