fe077dac3408b8ae18da295942a6f4c4ca14a90b
Tomas Schlosser <tschloss@redhat.com>
2014-02-12 10:47:40 +0100

    BRMS: Audit log tests

--- tests/org.jboss.tools.drools.reddeer/src/org/jboss/tools/drools/reddeer/view/AuditView.java
+++ tests/org.jboss.tools.drools.reddeer/src/org/jboss/tools/drools/reddeer/view/AuditView.java
@@ -1,5 +1,17 @@
 package org.jboss.tools.drools.reddeer.view;
 
+import java.lang.reflect.Method;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.IViewReference;
+import org.eclipse.ui.PlatformUI;
+import org.jboss.reddeer.swt.api.TreeItem;
+import org.jboss.reddeer.swt.impl.toolbar.DefaultToolItem;
+import org.jboss.reddeer.swt.impl.tree.DefaultTree;
+import org.jboss.reddeer.swt.util.Display;
+import org.jboss.reddeer.swt.util.ResultRunnable;
 import org.jboss.reddeer.workbench.view.impl.WorkbenchView;
 
 public class AuditView extends WorkbenchView {
@@ -8,15 +20,52 @@ public class AuditView extends WorkbenchView {
         super("Drools", "Audit");
     }
 
-    public void openLog() {
-        
+    public void openLog(final String logLocation) {
+        // Using the toolbar, the file dialog is opened
+        // new DefaultToolItem("Open Log").click();
+
+        // Retrieves the AuditView object from all opened objects
+        final IViewPart part = Display.syncExec(new ResultRunnable<IViewPart>() {
+            public IViewPart run() {
+                for (IViewReference ref : PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getViewReferences()) {
+                    if (ref.getId().contains("AuditView")) {
+                        return ref.getView(false);
+                    }
+                }
+                return null;
+            }
+        });
+
+        // call the setLogFile method using reflection (to avoid Drools plugin dependency)
+        if (part != null) {
+            Display.syncExec(new Runnable() {
+                public void run() {
+                    try {
+                        Method m = part.getClass().getMethod("setLogFile", String.class);
+                        m.invoke(part, logLocation);
+                    } catch (ReflectiveOperationException ex) {
+                        throw new RuntimeException("Unable to set log file", ex);
+                    }
+                }
+            });
+        }
     }
 
     public void refreshLog() {
-        
+        new DefaultToolItem("Refresh Log").click();
     }
 
     public void clearLog() {
-        
+        new DefaultToolItem("Clear Log").click();
+    }
+
+    public List<String> getEvents() {
+        List<String> result = new LinkedList<String>();
+
+        for (TreeItem item : new DefaultTree().getItems()) {
+            result.add(item.getText());
+        }
+
+        return result;
     }
 }
---
+++ tests/org.jboss.tools.drools.ui.bot.test/src/org/jboss/tools/drools/ui/bot/test/functional/brms5/Brms5AuditLogTest.java
@@ -0,0 +1,64 @@
+package org.jboss.tools.drools.ui.bot.test.functional.brms5;
+
+import java.util.List;
+
+import org.jboss.reddeer.eclipse.jdt.ui.packageexplorer.PackageExplorer;
+import org.jboss.reddeer.junit.runner.RedDeerSuite;
+import org.jboss.reddeer.swt.impl.button.PushButton;
+import org.jboss.reddeer.swt.impl.menu.ContextMenu;
+import org.jboss.reddeer.swt.impl.text.LabeledText;
+import org.jboss.reddeer.swt.matcher.RegexMatchers;
+import org.jboss.reddeer.swt.wait.WaitUntil;
+import org.jboss.tools.drools.reddeer.perspective.DroolsPerspective;
+import org.jboss.tools.drools.reddeer.view.AuditView;
+import org.jboss.tools.drools.ui.bot.test.annotation.UseDefaultProject;
+import org.jboss.tools.drools.ui.bot.test.annotation.UsePerspective;
+import org.jboss.tools.drools.ui.bot.test.group.SmokeTest;
+import org.jboss.tools.drools.ui.bot.test.util.ApplicationIsTerminated;
+import org.jboss.tools.drools.ui.bot.test.util.RunUtility;
+import org.jboss.tools.drools.ui.bot.test.util.RuntimeVersion;
+import org.jboss.tools.drools.ui.bot.test.util.TestParent;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+
+@RunWith(RedDeerSuite.class)
+public class Brms5AuditLogTest extends TestParent {
+
+    public Brms5AuditLogTest() {
+        super(RuntimeVersion.BRMS_5);
+    }
+
+    @Test @Category(SmokeTest.class)
+    @UsePerspective(DroolsPerspective.class) @UseDefaultProject
+    public void testDefaultProject() {
+        RunUtility.runAsJavaApplication(DEFAULT_PROJECT_NAME, "src/main/java", "com.sample", "DroolsTest.java");
+        new WaitUntil(new ApplicationIsTerminated());
+
+        PackageExplorer explorer = new PackageExplorer();
+        explorer.open();
+        explorer.getProject(DEFAULT_PROJECT_NAME).select();
+
+        new ContextMenu(new RegexMatchers("Refresh.*").getMatchers()).select();
+
+        explorer = new PackageExplorer();
+        explorer.open();
+        explorer.getProject(DEFAULT_PROJECT_NAME).getProjectItem("test.log").select();
+
+        new ContextMenu(new RegexMatchers("Properties.*").getMatchers()).select();
+
+        String location = new LabeledText("Location:").getText();
+        new PushButton("Cancel").click();
+
+        AuditView view = new AuditView();
+        view.open();
+        view.openLog(location);
+
+        List<String> events = view.getEvents();
+        Assert.assertEquals("Wrong number of audit log events", 3, events.size());
+        Assert.assertTrue("Wrong event encountered", events.get(0).contains("Object inserted"));
+        Assert.assertTrue("Wrong event encountered", events.get(1).contains("Activation executed"));
+        Assert.assertTrue("Wrong event encountered", events.get(2).contains("Activation executed"));
+    }
+}
