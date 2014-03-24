d6abb8bff9dccf50d57319455a566e62520b9b10
Andrej Podhradsky <apodhrad@redhat.com>
2014-03-24 16:44:59 +0100

    SwitchYard: fixed http binding page
    focus is needed, otherwise the operation name isn't saved

--- tests/org.jboss.tools.switchyard.reddeer/src/org/jboss/tools/switchyard/reddeer/binding/OperationOptionsPage.java
+++ tests/org.jboss.tools.switchyard.reddeer/src/org/jboss/tools/switchyard/reddeer/binding/OperationOptionsPage.java
@@ -2,6 +2,8 @@ package org.jboss.tools.switchyard.reddeer.binding;
 
 import org.jboss.reddeer.eclipse.jface.wizard.WizardPage;
 import org.jboss.reddeer.swt.impl.combo.DefaultCombo;
+import org.jboss.reddeer.swt.util.Display;
+import org.jboss.reddeer.swt.util.ResultRunnable;
 
 public abstract class OperationOptionsPage<T> extends WizardPage {
 
@@ -13,12 +15,20 @@ public abstract class OperationOptionsPage<T> extends WizardPage {
 	@SuppressWarnings("unchecked")
 	public T setOperation(String operation) {
 		new DefaultCombo(0).setSelection(OPERATION_NAME);
+		Display.syncExec(new ResultRunnable<Boolean>() {
+
+			@Override
+			public Boolean run() {
+				return new DefaultCombo(1).getSWTWidget().forceFocus();
+			}
+		});
+
 		new DefaultCombo(1).setSelection(operation);
 		return (T) this;
 	}
 
 	public String getOperation() {
-		return new DefaultCombo(0).getText();
+		return new DefaultCombo(1).getText();
 	}
 
 }
