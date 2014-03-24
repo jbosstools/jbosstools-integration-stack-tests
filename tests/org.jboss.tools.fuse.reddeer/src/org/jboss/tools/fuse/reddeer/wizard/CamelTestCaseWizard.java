64222d49731cb013ea583467e62b6435868b3832
Tomas Sedmik <tsedmik@redhat.com>
2014-03-24 13:07:43 +0100

    Fuse: New test case for 'Run as Local Context'

---
+++ tests/org.jboss.tools.fuse.reddeer/src/org/jboss/tools/fuse/reddeer/wizard/CamelTestCaseWizard.java
@@ -0,0 +1,18 @@
+package org.jboss.tools.fuse.reddeer.wizard;
+
+import org.jboss.reddeer.eclipse.jface.wizard.NewWizardDialog;
+
+/**
+ * Wizard for creating a Camel Test Case.
+ * 
+ * @author tsedmik
+ */
+public class CamelTestCaseWizard extends NewWizardDialog {
+
+	public CamelTestCaseWizard() {
+		super("Fuse Tooling", "Camel Test Case");
+	}
+	
+	
+
+}
