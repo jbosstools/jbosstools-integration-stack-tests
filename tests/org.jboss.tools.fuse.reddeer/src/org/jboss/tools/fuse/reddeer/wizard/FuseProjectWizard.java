395c8c21c3d251b1d93707ee46120a7ce8585216
Tomas Sedmik <tsedmik@redhat.com>
2014-03-24 12:59:25 +0100

    Fuse: Fixed RedDeer API change (checkButtonEnabled)

--- tests/org.jboss.tools.fuse.reddeer/src/org/jboss/tools/fuse/reddeer/wizard/FuseProjectWizard.java
+++ tests/org.jboss.tools.fuse.reddeer/src/org/jboss/tools/fuse/reddeer/wizard/FuseProjectWizard.java
@@ -1,13 +1,16 @@
 package org.jboss.tools.fuse.reddeer.wizard;
 
 import org.jboss.reddeer.eclipse.jface.wizard.NewWizardDialog;
+import org.jboss.reddeer.swt.api.Button;
 import org.jboss.reddeer.swt.condition.JobIsRunning;
 import org.jboss.reddeer.swt.condition.ShellWithTextIsActive;
+import org.jboss.reddeer.swt.condition.WidgetIsEnabled;
 import org.jboss.reddeer.swt.impl.button.PushButton;
 import org.jboss.reddeer.swt.impl.shell.DefaultShell;
 import org.jboss.reddeer.swt.impl.table.DefaultTable;
 import org.jboss.reddeer.swt.impl.text.LabeledText;
 import org.jboss.reddeer.swt.wait.TimePeriod;
+import org.jboss.reddeer.swt.wait.WaitUntil;
 import org.jboss.reddeer.swt.wait.WaitWhile;
 
 /**
@@ -31,10 +34,13 @@ public class FuseProjectWizard extends NewWizardDialog {
 	
 	@Override
 	public void finish() {
+		
 		log.info("Finish wizard");
-
+		
 		String shellText = new DefaultShell().getText();
-		new PushButton("Finish").click();
+		Button button = new PushButton("Finish");
+		new WaitUntil(new WidgetIsEnabled(button));
+		button.click();
 
 		TimePeriod timeout = TimePeriod.getCustom(20 * 60 * 1000);
 		new WaitWhile(new ShellWithTextIsActive(shellText), timeout);
