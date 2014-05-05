36ff22e62c8b00d16093431b26429e321b76472b
Tomas Sedmik <tsedmik@redhat.com>
2014-05-05 10:35:45 +0200

    Fuse: Add smoke test

---
+++ tests/org.jboss.tools.fuse.reddeer/src/org/jboss/tools/fuse/reddeer/perspectives/FuseIntegrationPerspective.java
@@ -0,0 +1,15 @@
+package org.jboss.tools.fuse.reddeer.perspectives;
+
+import org.jboss.reddeer.eclipse.ui.perspectives.AbstractPerspective;
+
+/**
+ * Fuse Integration perspective implementation
+ * 
+ * @author tsedmik
+ */
+public class FuseIntegrationPerspective extends AbstractPerspective {
+
+	public FuseIntegrationPerspective() {
+		super("Fuse Integration ");
+	}
+}
