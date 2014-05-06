5b56985529a5f5cf7f722394057ef67a6c49a827
Marek Baluch <mbaluch@redhat.com>
2014-05-06 11:53:17 +0200

    Improved DefaultPropertiesView

---
+++ tests/org.jboss.tools.bpmn2.reddeer/src/org/jboss/tools/reddeer/matcher/WidgetWithClassName.java
@@ -0,0 +1,41 @@
+package org.jboss.tools.reddeer.matcher;
+
+import org.eclipse.swt.widgets.Widget;
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+
+/**
+  * 
+  */
+public class WidgetWithClassName extends BaseMatcher<Widget> {
+
+	private String simpleClassName;
+
+	/**
+	 * 
+	 * @param simpleClassName
+	 */
+	public WidgetWithClassName(String simpleClassName) {
+		this.simpleClassName = simpleClassName;
+	}
+
+	/**
+	 * 
+	 * @param clazz
+	 */
+	public WidgetWithClassName(Class<?> clazz) {
+		this(clazz.getSimpleName());
+	}
+
+	@Override
+	public boolean matches(Object item) {
+		return item.getClass().getSimpleName().equals(simpleClassName);
+	}
+
+	@Override
+	public void describeTo(Description description) {
+		description.appendText("of class '").appendText(simpleClassName)
+				.appendText("'");
+	}
+
+}
