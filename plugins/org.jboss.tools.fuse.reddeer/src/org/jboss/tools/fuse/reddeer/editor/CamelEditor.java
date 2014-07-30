ec278bf2e2820415342f621a82f3d64467c4076c
Tomas Sedmik <tsedmik@redhat.com>
2014-07-30 17:58:53 +0200

    Fuse: Refactor 'CamelEditorTest'

--- plugins/org.jboss.tools.fuse.reddeer/src/org/jboss/tools/fuse/reddeer/editor/CamelEditor.java
+++ plugins/org.jboss.tools.fuse.reddeer/src/org/jboss/tools/fuse/reddeer/editor/CamelEditor.java
@@ -1,52 +1,29 @@
 package org.jboss.tools.fuse.reddeer.editor;
 
-import java.util.List;
-
-import org.eclipse.gef.EditPart;
-import org.eclipse.gef.GraphicalViewer;
-import org.jboss.reddeer.swt.handler.WidgetHandler;
-import org.jboss.reddeer.swt.wait.AbstractWait;
-import org.jboss.reddeer.swt.wait.TimePeriod;
+import org.jboss.reddeer.gef.editor.GEFEditor;
+import org.jboss.reddeer.gef.impl.editpart.LabeledEditPart;
+import org.jboss.reddeer.swt.impl.menu.ContextMenu;
 import org.jboss.tools.fuse.reddeer.component.CamelComponent;
-import org.jboss.tools.fuse.reddeer.editor.matcher.All;
 
 /**
+ * Manipulates with Camel Editor
  * 
- * @author apodhrad
- * 
+ * @author tsedmik
  */
-public class CamelEditor extends GefEditor {
-
-	public static final int MAX = 10;
-
-	protected GraphicalViewer viewer;
+public class CamelEditor extends GEFEditor {
 
 	public CamelEditor(String title) {
 		super(title);
 	}
 
 	public void addCamelComponent(CamelComponent component) {
-		addCamelComponent(component, 1);
-	}
 
-	protected void addCamelComponent(CamelComponent component, int count) {
-		if (count > MAX) {
-			throw new RuntimeException("Cannot add component '" + component.getPaletteEntry() + "'");
-		}
-		List<EditPart> before = getEditParts(new All());
-		getPalette().activateTool(component.getPaletteEntry());
-		AbstractWait.sleep(TimePeriod.getCustom(2));
-		WidgetHandler.getInstance().setFocus(getFigureCanvas());
-		click(0, 0);
-		WidgetHandler.getInstance().sendClickNotifications(getFigureCanvas());
-		AbstractWait.sleep(TimePeriod.getCustom(2));
-		List<EditPart> after = getEditParts(new All());
-		if (after.size() <= before.size()) {
-			addCamelComponent(component, count + 1);
-		}
+		addToolFromPalette(component.getPaletteEntry(), 0, 0);
 	}
 
 	public void deleteCamelComponent(CamelComponent component) {
-		deleteEditPartWithLabel(component.getLabel());
+
+		new LabeledEditPart(component.getLabel()).select();
+		new ContextMenu("Remove").select();
 	}
 }
--- tests/org.jboss.tools.fuse.ui.bot.test/src/org/jboss/tools/fuse/ui/bot/test/CamelEditorTest.java
+++ tests/org.jboss.tools.fuse.ui.bot.test/src/org/jboss/tools/fuse/ui/bot/test/CamelEditorTest.java
@@ -10,13 +10,9 @@ import org.jboss.reddeer.swt.impl.shell.WorkbenchShell;
 import org.jboss.tools.fuse.reddeer.component.CamelComponent;
 import org.jboss.tools.fuse.reddeer.component.CamelComponents;
 import org.jboss.tools.fuse.reddeer.editor.CamelEditor;
-import org.jboss.tools.fuse.reddeer.editor.GefEditor;
 import org.jboss.tools.fuse.reddeer.projectexplorer.CamelProject;
 import org.jboss.tools.fuse.ui.bot.test.utils.ProjectFactory;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.TestWatcher;
-import org.junit.runner.Description;
 import org.junit.runner.RunWith;
 
 /**
@@ -30,9 +26,6 @@ import org.junit.runner.RunWith;
 public class CamelEditorTest {
 
 	protected Logger log = Logger.getLogger(CamelEditorTest.class);
-
-	@Rule
-	public GefWatchdog gefWatchdog = new GefWatchdog();
 
 	@Test
 	public void camelEditorTest() {
@@ -52,18 +45,6 @@ public class CamelEditorTest {
 			CamelEditor editor = new CamelEditor("camel-context.xml");
 			editor.addCamelComponent(component);
 			editor.deleteCamelComponent(component);
-			editor.close();
-		}
-
-		System.out.println();
-	}
-
-	public class GefWatchdog extends TestWatcher {
-
-		@Override
-		protected void failed(Throwable e, Description description) {
-			new GefEditor().printAllFigures();
-			super.failed(e, description);
 		}
 	}
 }
