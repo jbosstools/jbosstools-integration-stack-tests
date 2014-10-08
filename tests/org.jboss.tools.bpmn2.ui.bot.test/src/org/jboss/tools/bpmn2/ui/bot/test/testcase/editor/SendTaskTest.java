9e9b8a2f9b9510cd311a74ba7fe70dcb1515ffba
Jozef Marko <jomarko@redhat.com>
2014-10-08 14:12:07 +0200

    BPMN2: adaptation for JBDSIS7.0.3, because of bug 1079699

--- tests/org.jboss.tools.bpmn2.ui.bot.test/src/org/jboss/tools/bpmn2/ui/bot/test/testcase/editor/SendTaskTest.java
+++ tests/org.jboss.tools.bpmn2.ui.bot.test/src/org/jboss/tools/bpmn2/ui/bot/test/testcase/editor/SendTaskTest.java
@@ -26,9 +26,9 @@ public class SendTaskTest extends JBPM6BaseTest {
 		start.append("Send", ElementType.SEND_TASK);
 
 		SendTask send = new SendTask("Send");
+		send.addParameterMapping(new ParameterMapping(new FromVariable("BPMN2-SendTask/s"), new ToDataInput("Message"), ParameterMapping.Type.INPUT));
 		send.setImplementation("Unspecified");
 		send.setMessage("_2_Message", "String");
-		send.addParameterMapping(new ParameterMapping(new FromVariable("BPMN2-SendTask/s"), new ToDataInput("Message"), ParameterMapping.Type.INPUT));
 		send.append("EndProcess", ElementType.TERMINATE_END_EVENT);
 	}
 	
