b3f6f5765b91a687d40922976746b692780fb059
Tomas Sedmik <tsedmik@redhat.com>
2014-10-22 16:44:36 +0200

    ESB: Add test for 'Project Examples'

---
+++ plugins/org.jboss.tools.esb.reddeer/src/org/jboss/tools/esb/reddeer/wizard/ProjectExamples.java
@@ -0,0 +1,83 @@
+package org.jboss.tools.esb.reddeer.wizard;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jboss.reddeer.jface.wizard.NewWizardDialog;
+import org.jboss.reddeer.swt.api.TreeItem;
+import org.jboss.reddeer.swt.condition.ShellWithTextIsAvailable;
+import org.jboss.reddeer.swt.impl.button.CheckBox;
+import org.jboss.reddeer.swt.impl.button.PushButton;
+import org.jboss.reddeer.swt.impl.shell.DefaultShell;
+import org.jboss.reddeer.swt.impl.tree.DefaultTree;
+import org.jboss.reddeer.swt.impl.tree.DefaultTreeItem;
+import org.jboss.reddeer.swt.wait.TimePeriod;
+import org.jboss.reddeer.swt.wait.WaitUntil;
+
+/**
+ * Wizard for creating project examples
+ * 
+ * @author tsedmik
+ */
+public class ProjectExamples extends NewWizardDialog {
+
+	private final static String DIALOG_TITLE = "New Project Example";
+	private final static String[] MENU_PATH = {"Examples", "JBoss Tools", "Project Examples"};
+
+	public ProjectExamples() {
+		super(MENU_PATH);
+	}
+
+	@Override
+	public void open() {
+		super.open();
+		new WaitUntil(new ShellWithTextIsAvailable(DIALOG_TITLE), TimePeriod.NORMAL);
+		new DefaultShell(DIALOG_TITLE);
+	}
+
+	@Override
+	public void finish() {
+		super.finish();
+		new WaitUntil(new ShellWithTextIsAvailable(DIALOG_TITLE), TimePeriod.getCustom(600));
+		if (new CheckBox(0).isEnabled()) {
+			new CheckBox(0).toggle(false);
+		}
+		new CheckBox(1).toggle(false);
+		new PushButton("Finish").click();
+	}
+
+	/**
+	 * Selects a given project
+	 * 
+	 * @param path Path to the given project
+	 */
+	public void selectProject(String[] path) {
+		new DefaultTreeItem(0, path).select();
+	}
+
+	/**
+	 * Accesses project examples related to JBoss ESB
+	 * 
+	 * @param version Version of used SOA Platform ('5.0')
+	 * @return List of the ESB projects examples
+	 */
+	public List<String[]> getESBProjects(String version) {
+
+		List<TreeItem> topItems = new ArrayList<TreeItem>();
+		List<String[]> result = new ArrayList<String[]>();
+		for (TreeItem item : new DefaultTree().getItems()) {
+			if (item.getText().startsWith("ESB") && item.getText().endsWith(version)) {
+				topItems.add(item);
+			}
+		}
+		for (TreeItem item : topItems) {
+			for (TreeItem subItem : item.getItems()) {
+				String[] temp = new String[2];
+				temp[0] = item.getText();
+				temp[1] = subItem.getText();
+				result.add(temp);
+			}
+		}
+		return result;
+	}
+}
---
+++ tests/org.jboss.tools.esb.ui.bot.test/src/org/jboss/tools/esb/ui/bot/test/ProjectExamplesTest.java
@@ -0,0 +1,81 @@
+package org.jboss.tools.esb.ui.bot.test;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.jboss.reddeer.eclipse.jdt.ui.ProjectExplorer;
+import org.jboss.reddeer.eclipse.ui.perspectives.JavaEEPerspective;
+import org.jboss.reddeer.eclipse.ui.problems.ProblemsView;
+import org.jboss.reddeer.junit.requirement.inject.InjectRequirement;
+import org.jboss.reddeer.junit.runner.RedDeerSuite;
+import org.jboss.reddeer.requirements.cleanworkspace.CleanWorkspaceRequirement.CleanWorkspace;
+import org.jboss.reddeer.requirements.openperspective.OpenPerspectiveRequirement.OpenPerspective;
+import org.jboss.reddeer.requirements.server.ServerReqState;
+import org.jboss.tools.esb.reddeer.wizard.ProjectExamples;
+import org.jboss.tools.runtime.reddeer.requirement.ServerReqType;
+import org.jboss.tools.runtime.reddeer.requirement.ServerRequirement;
+import org.jboss.tools.runtime.reddeer.requirement.ServerRequirement.Server;
+import org.junit.AfterClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Tests creation of ESB Project Examples (Help --> Project Examples...)
+ * 
+ * @author tsedmik
+ */
+@CleanWorkspace
+@Server(type = {ServerReqType.AS, ServerReqType.EAP}, state = ServerReqState.PRESENT)
+@OpenPerspective(JavaEEPerspective.class)
+@RunWith(RedDeerSuite.class)
+public class ProjectExamplesTest {
+
+	@InjectRequirement
+	ServerRequirement serverReq;
+
+	@AfterClass
+	public static void deleteAllProjects() {
+		new ProjectExplorer().deleteAllProjects();
+	}
+
+	@Test
+	public void testESBProjectExamples() {
+
+		List<String[]> projects;
+		ProjectExamples wizard = new ProjectExamples();
+		StringBuilder result = new StringBuilder();
+		
+		// get all available projects
+		wizard.open();
+		projects = wizard.getESBProjects("5.0");
+		wizard.cancel();
+		assertTrue(projects.size() > 0);
+
+		// try to create every available projects
+		for (String[] project : projects) {
+
+			// create projects
+			wizard = new ProjectExamples();
+			wizard.open();
+			wizard.selectProject(project);
+			wizard.finish();
+
+			// check projects
+			ProblemsView problemsView = new ProblemsView();
+			problemsView.open();
+			if (problemsView.getAllErrors().size() > 0) {
+				result.append(Arrays.toString(project));
+			}
+
+			// delete projects
+			new ProjectExplorer().deleteAllProjects();
+		}
+
+		if (result.length() > 0) {
+			fail("The following projects have some problems: " + result.toString());
+		}
+	}
+}
