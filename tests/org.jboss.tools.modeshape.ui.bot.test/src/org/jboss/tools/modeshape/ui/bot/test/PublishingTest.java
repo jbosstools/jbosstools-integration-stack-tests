ef121cc3b0b7c6c3408fd1f91b9a93b732adb9d6
Tomas Sedmik <tsedmik@redhat.com>
2015-01-06 10:38:32 +0100

    ModeShape: Adapt tests to JBDSIS 8.0.0 and DV 6.1.0 #201

--- tests/org.jboss.tools.modeshape.ui.bot.test/src/org/jboss/tools/modeshape/ui/bot/test/PublishingTest.java
+++ tests/org.jboss.tools.modeshape.ui.bot.test/src/org/jboss/tools/modeshape/ui/bot/test/PublishingTest.java
@@ -26,7 +26,6 @@ import org.junit.runner.RunWith;
  * Bot test for publishing/unpublishing files into/from ModeShape repository.
  * 
  * @author apodhrad
- * 
  */
 @CleanWorkspace
 @OpenPerspective(JavaPerspective.class)
@@ -39,60 +38,59 @@ public class PublishingTest {
 	public static final String FILE_CONTENT = "testcontent";
 
 	public static final String SERVER_URL = "http://localhost:8080/modeshape-rest";
-	public static final String USER = "admin";
-	public static final String PASSWORD = "admin";
-	public static final String PUBLISH_AREA = "/files";
+	public static final String USER = "modeshapeUser";
+	public static final String PASSWORD = "dvdvdv0!";
+	public static final String PUBLISH_AREA = "files";
 	public static final String WORKSPACE = "default";
-	
+
 	@Test
 	public void publishingTest() throws Exception {
-		
+
 		/* Create Java Project */
 		NewJavaProjectWizardDialog projectWizard = new NewJavaProjectWizardDialog();
 		projectWizard.open();
 		projectWizard.getFirstPage().setProjectName(PROJECT_NAME);
 		projectWizard.finish();
-		
+
 		/* Create Text File */
 		NewFileCreationWizardDialog fileWizard = new NewFileCreationWizardDialog();
 		fileWizard.open();
 		fileWizard.getFirstPage().setFolderPath(PROJECT_NAME);
 		fileWizard.getFirstPage().setFileName(FILE_NAME);
 		fileWizard.finish();
-		
+
 		/* Edit Text File */
 		TextEditor editor = new TextEditor(FILE_NAME);
 		editor.setText(FILE_CONTENT);
 		editor.save();
 		editor.close();
-		
+
 		/* Create ModeSHape Server */
-		new ModeshapeView().addServer(SERVER_URL, USER, PASSWORD);//admin=admin in modeshape-users.properties;admin=admin,connect,readonly,readwrite in modeshape-roles.properties
-		
+		new ModeshapeView().addServer(SERVER_URL, USER, PASSWORD);
+
 		/* Get ModeSHape Repository */
 		String repository = ModeshapeSuite.getModeshapeRepository();
-		
+
 		/* Add publish area */
 		new ModeshapeView().addPublishArea(SERVER_URL, repository, WORKSPACE, PUBLISH_AREA);
-		System.out.println("DEBUG: publishing area " + PUBLISH_AREA +" added");
-		
+		System.out.println("DEBUG: publishing area " + PUBLISH_AREA + " added");
+
 		/* Publish */
 		new ModeshapeExplorer().publish(PROJECT_NAME).finish();
-		assertTrue(new ModeshapeWebdav(repository).isFileAvailable(PROJECT_NAME + "/" + FILE_NAME, USER, PASSWORD));
+		assertTrue(new ModeshapeWebdav(SERVER_URL + "/v1", repository, WORKSPACE + "/items", PUBLISH_AREA).isFileAvailable(FILE_NAME, USER, PASSWORD));
 		System.out.println("DEBUG: files published (webdav)");
-		
+
 		/* Published Locations */
 		PublishedLocations locations = new ModeshapeExplorer().showPublishedLocations(PROJECT_NAME, FILE_NAME);
 		String url = locations.getPublishedUrl();
 		locations.ok();
 		assertTrue(new ModeshapeWebdav(repository).isFileAvailable(new URL(url), USER, PASSWORD));
 		System.out.println("DEBUG: files published (url)");
-		
+
 		/* Unpublish */
 		new ModeshapeExplorer().unpublish(PROJECT_NAME).finish();
-		assertFalse(new ModeshapeWebdav(repository).isFileAvailable(PROJECT_NAME + "/" + FILE_NAME, USER, PASSWORD));
+		assertFalse(new ModeshapeWebdav(SERVER_URL + "/v1", repository, WORKSPACE + "/items", PUBLISH_AREA).isFileAvailable(PROJECT_NAME + "/" + FILE_NAME, USER, PASSWORD));
 		System.out.println("DEBUG: files unpublished");
-		
 	}
-
 }
+
