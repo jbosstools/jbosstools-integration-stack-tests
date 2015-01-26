1bc70e3aa45b15e2470fc321cf161cf8682ca77f
Andrej Podhradsky <apodhrad@redhat.com>
2015-01-26 09:17:14 +0100

    jBPM3: Extend jBPM3 smoke tests

--- tests/org.jboss.tools.jbpm.ui.bot.test/src/org/jboss/tools/jbpm/ui/bot/test/suite/SmokeTests.java
+++ tests/org.jboss.tools.jbpm.ui.bot.test/src/org/jboss/tools/jbpm/ui/bot/test/suite/SmokeTests.java
@@ -3,12 +3,18 @@ package org.jboss.tools.jbpm.ui.bot.test.suite;
 import junit.framework.TestSuite;
 
 import org.jboss.reddeer.junit.runner.RedDeerSuite;
+import org.jboss.tools.jbpm.ui.bot.test.GPDPaletteTest;
+import org.jboss.tools.jbpm.ui.bot.test.GPDTest;
+import org.jboss.tools.jbpm.ui.bot.test.JBPMProjectTest;
 import org.jboss.tools.jbpm.ui.bot.test.PerspectiveTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite.SuiteClasses;
 
 @SuiteClasses({
 	PerspectiveTest.class,
+	JBPMProjectTest.class, 
+	GPDTest.class, 
+	GPDPaletteTest.class
 })
 @RunWith(RedDeerSuite.class)
 public class SmokeTests extends TestSuite {
