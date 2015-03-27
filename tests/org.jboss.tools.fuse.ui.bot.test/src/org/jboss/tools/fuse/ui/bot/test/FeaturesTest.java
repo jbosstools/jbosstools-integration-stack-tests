fffda50d16f7e0c88da8d302230f4837fe61693d
Tomas Sedmik <tsedmik@redhat.com>
2015-03-27 11:15:49 +0100

    Fuse: Add new test case covers FUSETOOLS-1274 #284

--- tests/org.jboss.tools.fuse.ui.bot.test/src/org/jboss/tools/fuse/ui/bot/test/FeaturesTest.java
+++ tests/org.jboss.tools.fuse.ui.bot.test/src/org/jboss/tools/fuse/ui/bot/test/FeaturesTest.java
@@ -1,17 +1,36 @@
 package org.jboss.tools.fuse.ui.bot.test;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import org.jboss.reddeer.eclipse.jdt.ui.ProjectExplorer;
 import org.jboss.reddeer.gef.impl.editpart.LabeledEditPart;
 import org.jboss.reddeer.junit.runner.RedDeerSuite;
 import org.jboss.reddeer.requirements.cleanworkspace.CleanWorkspaceRequirement.CleanWorkspace;
 import org.jboss.reddeer.requirements.openperspective.OpenPerspectiveRequirement.OpenPerspective;
+import org.jboss.reddeer.swt.condition.ShellWithTextIsAvailable;
+import org.jboss.reddeer.swt.exception.SWTLayerException;
+import org.jboss.reddeer.swt.handler.ShellHandler;
+import org.jboss.reddeer.swt.impl.button.PushButton;
+import org.jboss.reddeer.swt.impl.menu.ContextMenu;
+import org.jboss.reddeer.swt.impl.shell.DefaultShell;
 import org.jboss.reddeer.swt.impl.shell.WorkbenchShell;
+import org.jboss.reddeer.swt.impl.text.DefaultText;
+import org.jboss.reddeer.swt.impl.text.LabeledText;
+import org.jboss.reddeer.swt.impl.toolbar.DefaultToolItem;
+import org.jboss.reddeer.swt.matcher.RegexMatcher;
+import org.jboss.reddeer.swt.matcher.WithTextMatcher;
+import org.jboss.reddeer.swt.matcher.WithTooltipTextMatcher;
+import org.jboss.reddeer.swt.wait.AbstractWait;
+import org.jboss.reddeer.swt.wait.TimePeriod;
+import org.jboss.reddeer.swt.wait.WaitUntil;
+import org.jboss.reddeer.swt.wait.WaitWhile;
 import org.jboss.tools.fuse.reddeer.editor.CamelEditor;
 import org.jboss.tools.fuse.reddeer.perspectives.FuseIntegrationPerspective;
 import org.jboss.tools.fuse.reddeer.preference.FuseToolingEditorPreferencePage;
 import org.jboss.tools.fuse.reddeer.projectexplorer.CamelProject;
 import org.jboss.tools.fuse.ui.bot.test.utils.ProjectFactory;
+import org.junit.After;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -32,12 +51,21 @@ public class FeaturesTest {
 		new WorkbenchShell().maximize();
 	}
 
+	@After
+	public void clean() {
+
+		new WorkbenchShell();
+		new DefaultToolItem(new WorkbenchShell(), 0, new WithTooltipTextMatcher(new RegexMatcher("Save All.*"))).click();
+		new ProjectExplorer().deleteAllProjects();
+		ShellHandler.getInstance().closeAllNonWorbenchShells();
+	}
+
 	/**
 	 * Graphical Editor - Add option to configure if labels should be shown or not
 	 * https://issues.jboss.org/browse/FUSETOOLS-837
 	 */
 	@Test
-	public void testLabelOrIdInEditor() {
+	public void test837() {
 
 		ProjectFactory.createProject("camel-spring", "camel-archetype-spring");
 		new CamelProject("camel-spring").openCamelContext("camel-context.xml");
@@ -67,5 +95,41 @@ public class FeaturesTest {
 		} catch (Exception e) {
 			fail("'From' endpoint should be named after uri value 'file:src/data?noo...'");
 		}
+	}
+
+	/**
+	 * Add Context Menu to Camel Contexts folder
+	 * https://issues.jboss.org/browse/FUSETOOLS-1274
+	 */
+	@Test
+	public void test1274() {
+
+		ProjectFactory.createProject("camel-blueprint", "camel-archetype-blueprint");
+		new ProjectExplorer().getProject("camel-blueprint").getProjectItem("Camel Contexts").select();
+		AbstractWait.sleep(TimePeriod.SHORT);
+
+		// 1. check context menu item "New Camel XML File"
+		try {
+			new ContextMenu("New Camel XML File");
+		} catch (SWTLayerException ex){
+			fail("'Camel Contexts' does not have a context menu entry 'New Camel XML File'!");
+		}
+
+		// 2. check "RouteContainer"
+		new ContextMenu("New Camel XML File").select();
+		new WaitUntil(new ShellWithTextIsAvailable("New Camel Context XML File"));
+		new DefaultShell("New Camel Context XML File");
+		assertTrue(new LabeledText("RouteContainer:").getText().contains("src/main/resources/OSGI-INF/blueprint"));
+		new PushButton("Finish").click();
+		new WaitWhile(new ShellWithTextIsAvailable("New Camel Context XML File"));
+		
+		// 3. check that wizard didn't overwrite existing file
+		new ProjectExplorer().getProject("camel-blueprint").getProjectItem("Camel Contexts").select();
+		AbstractWait.sleep(TimePeriod.SHORT);
+		new ContextMenu("New Camel XML File").select();
+		new WaitUntil(new ShellWithTextIsAvailable("New Camel Context XML File"));
+		new DefaultShell("New Camel Context XML File");
+		new DefaultText(new WithTextMatcher(new RegexMatcher(" A file with that name.*")));
+		new PushButton("Cancel").click();
 	}
 }
