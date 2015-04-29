95a338257e48807a6526184c9d5c093105645e42
Tomas Sedmik <tsedmik@redhat.com>
2015-04-29 12:44:51 +0200

    Fuse: Fix 'DeploymentTest' #327

--- tests/org.jboss.tools.fuse.ui.bot.test/src/org/jboss/tools/fuse/ui/bot/test/DeploymentTest.java
+++ tests/org.jboss.tools.fuse.ui.bot.test/src/org/jboss/tools/fuse/ui/bot/test/DeploymentTest.java
@@ -2,19 +2,16 @@ package org.jboss.tools.fuse.ui.bot.test;
 
 import static org.junit.Assert.assertTrue;
 
-import org.jboss.reddeer.eclipse.jdt.ui.ProjectExplorer;
 import org.jboss.reddeer.junit.requirement.inject.InjectRequirement;
 import org.jboss.reddeer.junit.runner.RedDeerSuite;
 import org.jboss.reddeer.requirements.cleanworkspace.CleanWorkspaceRequirement.CleanWorkspace;
 import org.jboss.reddeer.requirements.openperspective.OpenPerspectiveRequirement.OpenPerspective;
 import org.jboss.reddeer.requirements.server.ServerReqState;
-import org.jboss.reddeer.swt.impl.shell.DefaultShell;
 import org.jboss.reddeer.swt.impl.shell.WorkbenchShell;
 import org.jboss.reddeer.swt.wait.AbstractWait;
 import org.jboss.reddeer.swt.wait.TimePeriod;
 import org.jboss.tools.fuse.reddeer.perspectives.FuseIntegrationPerspective;
 import org.jboss.tools.fuse.reddeer.server.ServerManipulator;
-import org.jboss.tools.fuse.reddeer.view.ErrorLogView;
 import org.jboss.tools.fuse.reddeer.view.FuseShell;
 import org.jboss.tools.fuse.ui.bot.test.utils.ProjectFactory;
 import org.jboss.tools.runtime.reddeer.impl.ServerFuse;
@@ -22,7 +19,6 @@ import org.jboss.tools.runtime.reddeer.requirement.ServerReqType;
 import org.jboss.tools.runtime.reddeer.requirement.ServerRequirement;
 import org.jboss.tools.runtime.reddeer.requirement.ServerRequirement.Server;
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -36,76 +32,61 @@ import org.junit.runner.RunWith;
 @CleanWorkspace
 @OpenPerspective(FuseIntegrationPerspective.class)
 @RunWith(RedDeerSuite.class)
-public class DeploymentTest {
+public class DeploymentTest extends DefaultTest {
 
 	private static final String PROJECT_ARCHETYPE = "camel-archetype-blueprint";
 	private static final String PROJECT_NAME = "camel-blueprint";
 	private static final String PROJECT2_ARCHETYPE = "camel-archetype-spring-dm";
 	private static final String PROJECT2_NAME = "camel-spring-dm";
-
+	private static final String TEST_SERVER = "TEST";
+	private static final String PROJECT_IS_DEPLOYED = "Route: timerToLog started and consuming from: Endpoint[timer://foo?period=5000";
+	private static final String PROJECT_IS_UNDEPLOYED = "(CamelContext: camel-1) is shutdown";
+	private static final String PROJECT2_IS_DEPLOYED = "Publishing application context as OSGi service with properties {org.springframework.context.service.name=camel-spring-dm";
+	
 	@InjectRequirement
 	private static ServerRequirement serverRequirement;
 
+	private ServerFuse fuse = (ServerFuse) serverRequirement.getConfig().getServerBase();
+	
 	@BeforeClass
 	public static void setUp() {
 
-		new WorkbenchShell().setFocus();
-		new WorkbenchShell().maximize();
 		ProjectFactory.createProject(PROJECT_NAME, PROJECT_ARCHETYPE);
 		ProjectFactory.createProject(PROJECT2_NAME, PROJECT2_ARCHETYPE);
-		new ErrorLogView().deleteLog();
-	}
-
-	@AfterClass
-	public static void cleanUp() {
-
-		new WorkbenchShell();
-		new ProjectExplorer().deleteAllProjects();
-		ServerManipulator.clean(serverRequirement.getConfig().getName());
 	}
 
 	@After
 	public void manageServers() {
 
-		new DefaultShell();
+		new WorkbenchShell().setFocus();
 		ServerManipulator.removeAllModules(serverRequirement.getConfig().getName());
 		ServerManipulator.stopServer(serverRequirement.getConfig().getName());
-		ServerManipulator.removeAllModules("TEST");
-		ServerManipulator.stopServer("TEST");
-		ServerManipulator.removeServer("TEST");
-		new ErrorLogView().deleteLog();
+		ServerManipulator.removeAllModules(TEST_SERVER);
+		ServerManipulator.stopServer(TEST_SERVER);
+		ServerManipulator.removeServer(TEST_SERVER);
 	}
 
 	@Test
 	public void testServerCreationDeployment() {
 
-		ServerFuse fuse = (ServerFuse) serverRequirement.getConfig().getServerBase();
-		ServerManipulator.addServer(fuse.getServerType(), fuse.getHost(), "TEST", fuse.getPort(), fuse.getUsername(),
-				fuse.getPassword(), PROJECT_NAME);
-		assertTrue(ServerManipulator.hasServerModule("TEST", PROJECT_NAME));
-		ServerManipulator.startServer("TEST");
-		assertTrue(new FuseShell().execute("log:display").contains(
-				"Route: timerToLog started and consuming from: Endpoint[timer://foo?period=5000"));
-		ServerManipulator.removeAllModules("TEST");
-		AbstractWait.sleep(TimePeriod.getCustom(20));
-		assertTrue(new FuseShell().execute("log:display").contains("(CamelContext: blueprintContext) is shutdown"));
-		assertTrue(new ErrorLogView().getErrorMessages().size() == 0);
+		ServerManipulator.addServer(fuse.getServerType(), fuse.getHost(), TEST_SERVER, fuse.getPort(), fuse.getUsername(), fuse.getPassword(), PROJECT_NAME);
+		assertTrue(ServerManipulator.hasServerModule(TEST_SERVER, PROJECT_NAME));
+		ServerManipulator.startServer(TEST_SERVER);
+		assertTrue(new FuseShell().containsLog(PROJECT_IS_DEPLOYED));
+		ServerManipulator.removeAllModules(TEST_SERVER);
+		AbstractWait.sleep(TimePeriod.getCustom(10));
+		assertTrue(new FuseShell().containsLog(PROJECT_IS_UNDEPLOYED));
 	}
 
 	@Test
 	public void testImmeadiatelyPublishing() {
 
-		String server = serverRequirement.getConfig().getName();
-
-		ServerManipulator.startServer(server);
-		ServerManipulator.setImmeadiatelyPublishing(server, false);
-		ServerManipulator.addModule(server, PROJECT2_NAME);
-		assertTrue(ServerManipulator.hasServerModule(server, PROJECT2_NAME));
-		ServerManipulator.publish(server);
+		ServerManipulator.startServer(fuse.getName());
+		ServerManipulator.setImmeadiatelyPublishing(fuse.getName(), false);
+		ServerManipulator.addModule(fuse.getName(), PROJECT2_NAME);
+		assertTrue(ServerManipulator.hasServerModule(fuse.getName(), PROJECT2_NAME));
+		ServerManipulator.publish(fuse.getName());
 		AbstractWait.sleep(TimePeriod.NORMAL);
-		assertTrue(new FuseShell()
-				.execute("log:display")
-				.contains("Publishing application context as OSGi service with properties {org.springframework.context.service.name=camel-spring-dm"));
-		assertTrue(new ErrorLogView().getErrorMessages().size() == 0);
+		assertTrue(new FuseShell().containsLog(PROJECT2_IS_DEPLOYED));
 	}
 }
