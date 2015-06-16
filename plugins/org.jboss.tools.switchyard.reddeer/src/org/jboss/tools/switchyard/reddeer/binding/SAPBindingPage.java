41a8815b8df579f3bd6ec18787b45191921a4c02
Andrej Podhradsky <apodhrad@redhat.com>
2015-06-16 16:35:44 +0200

    SwitchYard: Add test for SAP component #386

--- plugins/org.jboss.tools.switchyard.reddeer/src/org/jboss/tools/switchyard/reddeer/binding/SAPBindingPage.java
+++ plugins/org.jboss.tools.switchyard.reddeer/src/org/jboss/tools/switchyard/reddeer/binding/SAPBindingPage.java
@@ -3,6 +3,7 @@ package org.jboss.tools.switchyard.reddeer.binding;
 import org.jboss.reddeer.swt.api.Combo;
 import org.jboss.reddeer.swt.api.Text;
 import org.jboss.reddeer.swt.impl.combo.LabeledCombo;
+import org.jboss.reddeer.swt.impl.group.DefaultGroup;
 import org.jboss.reddeer.swt.impl.text.LabeledText;
 
 /**
@@ -13,7 +14,11 @@ import org.jboss.reddeer.swt.impl.text.LabeledText;
  */
 public class SAPBindingPage extends OperationOptionsPage<SAPBindingPage> {
 
+	public static final String SAP_OBJECT_IDOC = "IDoc";
 	public static final String SAP_OBJECT_IDOC_LIST = "IDoc List";
+	public static final String SAP_OBJECT_QIDOC = "QIDoc";
+	public static final String SAP_OBJECT_QIDOC_LIST = "QIDoc List";
+	public static final String SAP_OBJECT_QRFC = "qRFC";
 	public static final String SAP_OBJECT_SRFC = "sRFC";
 	public static final String SAP_OBJECT_TRFC = "tRFC";
 
@@ -23,6 +28,10 @@ public class SAPBindingPage extends OperationOptionsPage<SAPBindingPage> {
 
 	public LabeledText getServer() {
 		return new LabeledText("Server*");
+	}
+
+	public Text getDestinationName() {
+		return new LabeledText("Destination Name*");
 	}
 
 	public Text getApplicationRelease() {
@@ -43,6 +52,30 @@ public class SAPBindingPage extends OperationOptionsPage<SAPBindingPage> {
 
 	public Combo getSAPObject() {
 		return new LabeledCombo("SAP Object");
+	}
+
+	public Text getQueueName() {
+		return new LabeledText("Queue Name");
+	}
+
+	public Combo getTransacted() {
+		return new LabeledCombo("Transacted");
+	}
+
+	public Text getExcludeNamespaces() {
+		return new LabeledText(new DefaultGroup("Regular Expressions"), "Exclude Namespaces");
+	}
+
+	public Text getIncludeNamespaces() {
+		return new LabeledText(new DefaultGroup("Regular Expressions"), "Include Namespaces");
+	}
+
+	public Text getExcludes() {
+		return new LabeledText(new DefaultGroup("Regular Expressions"), "Excludes");
+	}
+
+	public Text getIncludes() {
+		return new LabeledText(new DefaultGroup("Regular Expressions"), "Includes");
 	}
 
 }
