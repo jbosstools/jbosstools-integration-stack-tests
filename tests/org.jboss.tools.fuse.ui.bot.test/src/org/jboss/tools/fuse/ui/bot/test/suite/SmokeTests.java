1616b395095af3d4407f5c3b44e99f1efc74a405
Tomas Sedmik <tsedmik@redhat.com>
2015-07-27 14:28:16 +0200

    Fuse: Extend SmokeTests suite #398

--- tests/org.jboss.tools.fuse.ui.bot.test/src/org/jboss/tools/fuse/ui/bot/test/suite/SmokeTests.java
+++ tests/org.jboss.tools.fuse.ui.bot.test/src/org/jboss/tools/fuse/ui/bot/test/suite/SmokeTests.java
@@ -3,7 +3,9 @@ package org.jboss.tools.fuse.ui.bot.test.suite;
 import junit.framework.TestSuite;
 
 import org.jboss.reddeer.junit.runner.RedDeerSuite;
+import org.jboss.tools.fuse.ui.bot.test.JMXNavigatorTest;
 import org.jboss.tools.fuse.ui.bot.test.ProjectLocalRunTest;
+import org.jboss.tools.fuse.ui.bot.test.ServerTest;
 import org.jboss.tools.fuse.ui.bot.test.SmokeTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite.SuiteClasses;
@@ -14,8 +16,10 @@ import org.junit.runners.Suite.SuiteClasses;
  * @author tsedmik
  */
 @SuiteClasses({
-		SmokeTest.class,
-		ProjectLocalRunTest.class
+		JMXNavigatorTest.class,
+		ProjectLocalRunTest.class,
+		ServerTest.class,
+		SmokeTest.class
 })
 @RunWith(RedDeerSuite.class)
 public class SmokeTests extends TestSuite {
