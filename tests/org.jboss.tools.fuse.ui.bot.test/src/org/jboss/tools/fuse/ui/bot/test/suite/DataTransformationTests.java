6c58becb316c8eb9116f0d27e8a84fcab9d7f1da
Tomas Sedmik <tsedmik@redhat.com>
2015-08-04 10:14:39 +0200

    Fuse: Create a suite for Data Transformation related tests #442

---
+++ tests/org.jboss.tools.fuse.ui.bot.test/src/org/jboss/tools/fuse/ui/bot/test/suite/DataTransformationTests.java
@@ -0,0 +1,19 @@
+package org.jboss.tools.fuse.ui.bot.test.suite;
+
+import org.jboss.reddeer.junit.runner.RedDeerSuite;
+import org.jboss.tools.fuse.ui.bot.test.DataTransformationTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * Runs tests related to Data Transformation
+ * 
+ * @author tsedmik
+ */
+@SuiteClasses({
+	DataTransformationTest.class
+})
+@RunWith(RedDeerSuite.class)
+public class DataTransformationTests {
+
+}
