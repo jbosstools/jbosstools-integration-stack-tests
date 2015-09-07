0478ee9364ea014eee571fd5710642d3ecd6f1fe
Andrej Podhradsky <apodhrad@redhat.com>
2015-09-07 09:14:37 +0200

    Runtime: Add at least one test to SmokeTests #472

--- tests/org.jboss.tools.runtime.ui.bot.test/src/org/jboss/tools/runtime/ui/bot/test/suite/SmokeTests.java
+++ tests/org.jboss.tools.runtime.ui.bot.test/src/org/jboss/tools/runtime/ui/bot/test/suite/SmokeTests.java
@@ -1,10 +1,11 @@
 package org.jboss.tools.runtime.ui.bot.test.suite;
 
 import org.jboss.reddeer.junit.runner.RedDeerSuite;
+import org.jboss.tools.runtime.ui.bot.test.ServerPresentTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite.SuiteClasses;
 
-@SuiteClasses({})
+@SuiteClasses({ServerPresentTest.class})
 @RunWith(RedDeerSuite.class)
 public class SmokeTests {
 
