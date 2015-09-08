58bee8fa64c9c43fa0d389670b5fafde5217421d
Tomas Sedmik <tsedmik@redhat.com>
2015-09-08 12:55:23 +0200

    Fuse: Fix failing 'SmokeTests' #430

--- tests/org.jboss.tools.fuse.ui.bot.test/src/org/jboss/tools/fuse/ui/bot/test/suite/SmokeTests.java
+++ tests/org.jboss.tools.fuse.ui.bot.test/src/org/jboss/tools/fuse/ui/bot/test/suite/SmokeTests.java
@@ -1,14 +1,12 @@
 package org.jboss.tools.fuse.ui.bot.test.suite;
 
-import junit.framework.TestSuite;
-
 import org.jboss.reddeer.junit.runner.RedDeerSuite;
-import org.jboss.tools.fuse.ui.bot.test.JMXNavigatorTest;
 import org.jboss.tools.fuse.ui.bot.test.ProjectLocalRunTest;
-import org.jboss.tools.fuse.ui.bot.test.ServerTest;
 import org.jboss.tools.fuse.ui.bot.test.SmokeTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite.SuiteClasses;
+
+import junit.framework.TestSuite;
 
 /**
  * Runs smoke tests on Fuse Tooling
@@ -16,9 +14,7 @@ import org.junit.runners.Suite.SuiteClasses;
  * @author tsedmik
  */
 @SuiteClasses({
-		JMXNavigatorTest.class,
 		ProjectLocalRunTest.class,
-		ServerTest.class,
 		SmokeTest.class
 })
 @RunWith(RedDeerSuite.class)
