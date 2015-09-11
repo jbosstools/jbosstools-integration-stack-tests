6a4939bcaeb791b0eda51d03e0515a4deb003775
Tomas David <tdavid@redhat.com>
2015-09-11 12:32:20 +0200

    Drools: Added repository manipulation test

--- plugins/org.jboss.tools.drools.reddeer/src/org/jboss/tools/drools/reddeer/kienavigator/dialog/AddRepositoryDialog.java
+++ plugins/org.jboss.tools.drools.reddeer/src/org/jboss/tools/drools/reddeer/kienavigator/dialog/AddRepositoryDialog.java
@@ -1,10 +1,10 @@
 package org.jboss.tools.drools.reddeer.kienavigator.dialog;
 
-import org.jboss.reddeer.swt.impl.list.DefaultList;
+import org.jboss.reddeer.swt.impl.table.DefaultTable;
 
 public class AddRepositoryDialog extends Dialog {
 
 	public void selectRepository(String repository) {
-		new DefaultList(0).select(repository);
+		new DefaultTable().select(repository);
 	}
 }
