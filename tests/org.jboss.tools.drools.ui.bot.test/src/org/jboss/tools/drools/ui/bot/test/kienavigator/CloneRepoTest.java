dfce58ca9d4696f479b0434e178c7116c57af5db
Tomas David <tdavid@redhat.com>
2015-10-22 09:38:55 +0200

    Drools: moved init server config to parent class

--- tests/org.jboss.tools.drools.ui.bot.test/src/org/jboss/tools/drools/ui/bot/test/kienavigator/CloneRepoTest.java
+++ tests/org.jboss.tools.drools.ui.bot.test/src/org/jboss/tools/drools/ui/bot/test/kienavigator/CloneRepoTest.java
@@ -7,9 +7,6 @@ import org.jboss.reddeer.swt.impl.tree.DefaultTree;
 import org.jboss.tools.drools.reddeer.kienavigator.dialog.CreateOrgUnitDialog;
 import org.jboss.tools.drools.reddeer.kienavigator.dialog.CreateRepositoryDialog;
 import org.jboss.tools.drools.reddeer.kienavigator.item.RepositoryItem;
-import org.jboss.tools.drools.reddeer.kienavigator.item.ServerItem;
-import org.jboss.tools.drools.reddeer.kienavigator.properties.ServerProperties;
-import org.jboss.tools.drools.reddeer.view.KieNavigatorView;
 import org.jboss.tools.runtime.reddeer.requirement.ServerReqType;
 import org.jboss.tools.runtime.reddeer.requirement.ServerRequirement;
 import org.jboss.tools.runtime.reddeer.requirement.ServerRequirement.Server;
@@ -25,16 +22,7 @@ public class CloneRepoTest extends KieNavigatorTestParent {
 	private ServerRequirement serverReq;
 	
 	@Test
-	public void cloneRepoTest() { 
-		KieNavigatorView knv = new KieNavigatorView();
-		knv.open(); 
-		
-		ServerItem si = knv.getServers().get(0);
-		ServerProperties sp = si.properties();
-		setCorrectServerProperties(sp);
-		sp.apply();
-		sp.ok();
-		
+	public void cloneRepoTest() {
  		CreateOrgUnitDialog cod = knv.getServer(0).createOrgUnit();
 		cod.setName("clonename");
 		cod.setOwner("somebody");
