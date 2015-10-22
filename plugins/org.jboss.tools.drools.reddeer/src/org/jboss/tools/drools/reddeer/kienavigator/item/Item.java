5909ac16fc149a98b044ca68fb5f53f7db00838b
Tomas David <tdavid@redhat.com>
2015-10-22 08:36:25 +0200

    Drools: improved show in git action
    Signed-off-by: Tomas Sedmik <tsedmik@redhat.com>

--- plugins/org.jboss.tools.drools.reddeer/src/org/jboss/tools/drools/reddeer/kienavigator/item/Item.java
+++ plugins/org.jboss.tools.drools.reddeer/src/org/jboss/tools/drools/reddeer/kienavigator/item/Item.java
@@ -1,6 +1,7 @@
 package org.jboss.tools.drools.reddeer.kienavigator.item;
 
 import org.jboss.reddeer.swt.api.TreeItem;
+import org.jboss.reddeer.swt.impl.button.PushButton;
 import org.jboss.reddeer.swt.impl.menu.ContextMenu;
 import org.jboss.tools.drools.reddeer.kienavigator.properties.Properties;
 
@@ -19,6 +20,15 @@ public abstract class Item<T extends Properties> {
 	protected void selectAction(String actionName) {
 		treeItem.select();
 		new ContextMenu(actionName).select();
+	}
+	
+	protected void selectEnabledAction(String actionName) {
+		treeItem.select();
+		ContextMenu cm = new ContextMenu(actionName);
+		if (cm.isEnabled()) {
+			cm.select();
+			new PushButton("Yes").click();
+		}
 	}
 	
 	public abstract T properties();
