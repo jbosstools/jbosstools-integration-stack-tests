381cda7e8c158a4c5be003b6023a1a9c6d535208
Matus Makovy <mmakovy@redhat.com>
2015-12-07 15:12:42 +0100

    Added tests to SmokeTest + some clean-up in launchers

--- tests/org.jboss.tools.teiid.ui.bot.test/src/org/jboss/tools/teiid/ui/bot/test/suite/SmokeTests.java
+++ tests/org.jboss.tools.teiid.ui.bot.test/src/org/jboss/tools/teiid/ui/bot/test/suite/SmokeTests.java
@@ -1,12 +1,16 @@
 package org.jboss.tools.teiid.ui.bot.test.suite;
 
 import org.jboss.reddeer.junit.runner.RedDeerSuite;
+import org.jboss.tools.teiid.ui.bot.test.BasicTest;
+import org.jboss.tools.teiid.ui.bot.test.ModelWizardTest;
 import org.jboss.tools.teiid.ui.bot.test.PerspectiveTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite.SuiteClasses;
 
 @SuiteClasses({
-	PerspectiveTest.class
+	PerspectiveTest.class,
+	BasicTest.class,
+	ModelWizardTest.class
 })
 @RunWith(RedDeerSuite.class)
 public class SmokeTests {
