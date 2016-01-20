aff60ed4cca976db489a58e8bbce0f6289ae7160
Andrej Podhradsky <apodhrad@redhat.com>
2016-01-20 18:21:47 +0100

    ESB: Extend smoke tests #572

--- tests/org.jboss.tools.esb.ui.bot.test/src/org/jboss/tools/esb/ui/bot/test/suite/SmokeTests.java
+++ tests/org.jboss.tools.esb.ui.bot.test/src/org/jboss/tools/esb/ui/bot/test/suite/SmokeTests.java
@@ -3,11 +3,20 @@ package org.jboss.tools.esb.ui.bot.test.suite;
 import junit.framework.TestSuite;
 
 import org.jboss.reddeer.junit.runner.RedDeerSuite;
+import org.jboss.tools.esb.ui.bot.test.ActionsTest;
+import org.jboss.tools.esb.ui.bot.test.ListenersTest;
 import org.jboss.tools.esb.ui.bot.test.NewESBFileTest;
+import org.jboss.tools.esb.ui.bot.test.NotificationsTest;
+import org.jboss.tools.esb.ui.bot.test.ProvidersTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite.SuiteClasses;
 
-@SuiteClasses({ NewESBFileTest.class, })
+@SuiteClasses({
+	NewESBFileTest.class,
+	ActionsTest.class,
+	ListenersTest.class,
+	ProvidersTest.class,
+	NotificationsTest.class })
 @RunWith(RedDeerSuite.class)
 public class SmokeTests extends TestSuite {
 
