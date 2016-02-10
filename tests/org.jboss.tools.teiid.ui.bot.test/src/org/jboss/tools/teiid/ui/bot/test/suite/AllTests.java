efd43ada4aa70a4eff37033d47e9043bd5faf234
Matus Makovy <mmakovy@redhat.com>
2016-02-10 17:02:26 +0100

    Remove unused tests and update test suites

--- tests/org.jboss.tools.teiid.ui.bot.test/src/org/jboss/tools/teiid/ui/bot/test/suite/AllTests.java
+++ tests/org.jboss.tools.teiid.ui.bot.test/src/org/jboss/tools/teiid/ui/bot/test/suite/AllTests.java
@@ -1,18 +1,24 @@
 package org.jboss.tools.teiid.ui.bot.test.suite;
 
 import org.jboss.reddeer.junit.runner.RedDeerSuite;
+import org.jboss.tools.teiid.ui.bot.test.BasicTest;
+import org.jboss.tools.teiid.ui.bot.test.CloneAndCopyTest;
 import org.jboss.tools.teiid.ui.bot.test.ConsumeRestWs;
 import org.jboss.tools.teiid.ui.bot.test.ConsumeSoapWs;
 import org.jboss.tools.teiid.ui.bot.test.CreateRestProcedureTest;
+import org.jboss.tools.teiid.ui.bot.test.DynamicVdbTest;
 import org.jboss.tools.teiid.ui.bot.test.E2eRecursiveXmlTextTest;
 import org.jboss.tools.teiid.ui.bot.test.FlatFileTest;
+import org.jboss.tools.teiid.ui.bot.test.GuidesTest;
 import org.jboss.tools.teiid.ui.bot.test.ImportWizardTest;
 import org.jboss.tools.teiid.ui.bot.test.LdapImportTest;
 import org.jboss.tools.teiid.ui.bot.test.ModelRefactoringTest;
 import org.jboss.tools.teiid.ui.bot.test.ModelWizardTest;
+import org.jboss.tools.teiid.ui.bot.test.PreviewModelTest;
 import org.jboss.tools.teiid.ui.bot.test.ProcedurePreviewTest;
-import org.jboss.tools.teiid.ui.bot.test.RestCallTest;
 import org.jboss.tools.teiid.ui.bot.test.WARTest;
+import org.jboss.tools.teiid.ui.bot.test.XmlFileImportTest;
+import org.jboss.tools.teiid.ui.bot.test.XmlSchemalessTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite.SuiteClasses;
 
@@ -22,18 +28,24 @@ import org.junit.runners.Suite.SuiteClasses;
  * @author apodhrad, tsedmik
  */
 @SuiteClasses({
-	ImportWizardTest.class,
-	LdapImportTest.class,
-	ModelWizardTest.class,
-	ModelRefactoringTest.class,
-	CreateRestProcedureTest.class,
-	RestCallTest.class,
-	FlatFileTest.class,
-	ProcedurePreviewTest.class,
-	WARTest.class,
+	BasicTest.class,
+	CloneAndCopyTest.class,
 	ConsumeRestWs.class,
 	ConsumeSoapWs.class,
-	E2eRecursiveXmlTextTest.class, })
+	CreateRestProcedureTest.class,
+	DynamicVdbTest.class,
+	E2eRecursiveXmlTextTest.class,
+	FlatFileTest.class,
+	GuidesTest.class,
+	ImportWizardTest.class,
+	LdapImportTest.class,
+	ModelRefactoringTest.class,
+	ModelWizardTest.class,
+	PreviewModelTest.class,
+	ProcedurePreviewTest.class,
+	WARTest.class,
+	XmlFileImportTest.class,
+	XmlSchemalessTest.class})
 @RunWith(RedDeerSuite.class)
 public class AllTests {
 }
