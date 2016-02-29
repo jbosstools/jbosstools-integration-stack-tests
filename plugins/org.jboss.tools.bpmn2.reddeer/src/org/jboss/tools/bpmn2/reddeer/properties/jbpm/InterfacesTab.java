f1482dbb8f1c8d0e0ef41758bd657cd86426936b
Jozef Marko <jomarko@redhat.com>
2016-02-29 15:05:32 +0100

    BPMN2: fixed import interface test

--- plugins/org.jboss.tools.bpmn2.reddeer/src/org/jboss/tools/bpmn2/reddeer/properties/jbpm/InterfacesTab.java
+++ plugins/org.jboss.tools.bpmn2.reddeer/src/org/jboss/tools/bpmn2/reddeer/properties/jbpm/InterfacesTab.java
@@ -8,6 +8,7 @@ import org.jboss.reddeer.swt.impl.text.LabeledText;
 import org.jboss.reddeer.swt.impl.tree.DefaultTree;
 import org.jboss.reddeer.swt.impl.tree.DefaultTreeItem;
 import org.jboss.reddeer.uiforms.impl.section.DefaultSection;
+import org.jboss.tools.bpmn2.reddeer.editor.dialog.jbpm.ImportJavaTypeDialog;
 import org.jboss.tools.bpmn2.reddeer.editor.properties.SectionToolItem;
 
 /**
@@ -34,7 +35,11 @@ public class InterfacesTab {
 	public void importInterface(String fullQualifiedName) {
 		new SectionToolItem("Interface List", "Import").click();
 
-		broseForTypeForImport(fullQualifiedName);
+		new ImportJavaTypeDialog(false).add(fullQualifiedName);
+
+		String type = fullQualifiedName.substring(fullQualifiedName.lastIndexOf(".") + 1);
+
+		new DefaultTree(0).selectItems(new DefaultTreeItem(type + " - " + fullQualifiedName));
 
 		new PushButton("Select All").click();
 		new CheckBox(0).click();
