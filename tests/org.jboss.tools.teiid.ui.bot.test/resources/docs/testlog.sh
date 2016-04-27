18ee2dad525aa1c096764d4fe48029fcd026e37e
Matus Makovy <mmakovy@redhat.com>
2016-04-27 12:52:43 +0200

    JBTIS-685 - Clean-up resources folder

--- tests/org.jboss.tools.teiid.ui.bot.test/resources/docs/testlog.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ $# -lt 2 ]; then
-	echo "Usage: testlog.sh <TestName> <env>";
-	exit;
-fi
-
-A=`date +"%d-%m--%Y--%H:%M"`
-echo -e $1"\t"$2"\t"$A
-echo -e $1"\t"$2"\t"$A >> testlog
