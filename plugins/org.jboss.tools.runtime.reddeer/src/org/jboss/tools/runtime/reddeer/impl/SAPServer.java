7927b1351e7b02cd32135d590c048845ead7f27a
Andrej Podhradsky <apodhrad@redhat.com>
2016-05-24 14:17:43 +0200

    JBTIS-715: Add the attribute 'destination name' into SAP configuration

--- plugins/org.jboss.tools.runtime.reddeer/src/org/jboss/tools/runtime/reddeer/impl/SAPServer.java
+++ plugins/org.jboss.tools.runtime.reddeer/src/org/jboss/tools/runtime/reddeer/impl/SAPServer.java
@@ -2,6 +2,7 @@ package org.jboss.tools.runtime.reddeer.impl;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
@@ -11,8 +12,17 @@ import org.jboss.tools.runtime.reddeer.Namespaces;
 @XmlAccessorType(XmlAccessType.FIELD)
 public class SAPServer {
 
+	@XmlAttribute(name = "name")
+	private String name;
+
+	@XmlElement(name = "destination", namespace = Namespaces.SOA_REQ)
+	private String destination;
+
 	@XmlElement(name = "gwhost", namespace = Namespaces.SOA_REQ)
 	private String gwhost;
+
+	@XmlElement(name = "gwport", namespace = Namespaces.SOA_REQ)
+	private String gwport;
 
 	@XmlElement(name = "progid", namespace = Namespaces.SOA_REQ)
 	private String progid;
@@ -20,12 +30,36 @@ public class SAPServer {
 	@XmlElement(name = "connectionCount", namespace = Namespaces.SOA_REQ)
 	private String connectionCount;
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDestination() {
+		return destination;
+	}
+
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+
 	public String getGwhost() {
 		return gwhost;
 	}
 
 	public void setGwhost(String gwhost) {
 		this.gwhost = gwhost;
+	}
+
+	public String getGwport() {
+		return gwport;
+	}
+
+	public void setGwport(String gwport) {
+		this.gwport = gwport;
 	}
 
 	public String getProgid() {
