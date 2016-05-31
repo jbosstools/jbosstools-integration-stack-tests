570c54609f05608cf34ff887402f5fdcc7348234
Tomáš Sedmík <tsedmik@redhat.com>
2016-05-31 10:38:13 +0200

    Fix 'DataTransformationDeploymentEAPTest'
    Fuse Tooling related
    - tested with JBoss Fuse on EAP 6.3.0.ER1

--- tests/org.jboss.tools.fuse.ui.bot.test/src/org/jboss/tools/fuse/ui/bot/test/DeploymentEAPTest.java
+++ tests/org.jboss.tools.fuse.ui.bot.test/src/org/jboss/tools/fuse/ui/bot/test/DeploymentEAPTest.java
@@ -1,22 +1,30 @@
 package org.jboss.tools.fuse.ui.bot.test;
 
-import static org.jboss.reddeer.requirements.server.ServerReqState.RUNNING;
+import static org.jboss.reddeer.requirements.server.ServerReqState.PRESENT;
 import static org.jboss.tools.runtime.reddeer.requirement.ServerReqType.EAP;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.List;
+
+import org.jboss.reddeer.common.wait.TimePeriod;
 import org.jboss.reddeer.common.wait.WaitUntil;
 import org.jboss.reddeer.eclipse.condition.ConsoleHasText;
 import org.jboss.reddeer.eclipse.ui.browser.BrowserView;
+import org.jboss.reddeer.eclipse.wst.server.ui.RuntimePreferencePage;
+import org.jboss.reddeer.eclipse.wst.server.ui.view.ServersView;
 import org.jboss.reddeer.junit.requirement.inject.InjectRequirement;
 import org.jboss.reddeer.junit.runner.RedDeerSuite;
 import org.jboss.reddeer.requirements.openperspective.OpenPerspectiveRequirement.OpenPerspective;
+import org.jboss.reddeer.workbench.ui.dialogs.WorkbenchPreferenceDialog;
 import org.jboss.tools.fuse.reddeer.perspectives.FuseIntegrationPerspective;
 import org.jboss.tools.fuse.ui.bot.test.utils.FuseArchetypeNotFoundException;
 import org.jboss.tools.fuse.ui.bot.test.utils.ProjectFactory;
+import org.jboss.tools.runtime.reddeer.ServerBase;
 import org.jboss.tools.runtime.reddeer.requirement.ServerRequirement;
 import org.jboss.tools.runtime.reddeer.requirement.ServerRequirement.Server;
 import org.jboss.tools.runtime.reddeer.utils.FuseServerManipulator;
+import org.jboss.tools.runtime.reddeer.wizard.ServerRuntimeWizard;
 import org.junit.After;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -27,7 +35,7 @@ import org.junit.runner.RunWith;
  * 
  * @author tsedmik
  */
-@Server(type = EAP, state = RUNNING)
+@Server(type = EAP, state = PRESENT)
 @OpenPerspective(FuseIntegrationPerspective.class)
 @RunWith(RedDeerSuite.class)
 public class DeploymentEAPTest extends DefaultTest {
@@ -46,6 +54,29 @@ public class DeploymentEAPTest extends DefaultTest {
 	 */
 	@BeforeClass
 	public static void setupInitial() throws FuseArchetypeNotFoundException {
+
+		// change default runtime JRE -- due to https://issues.jboss.org/browse/JBTIS-719
+		ServerBase base = serverRequirement.getConfig().getServerBase(); 
+		if (base.getJreName() == null) {
+			WorkbenchPreferenceDialog preferences = new WorkbenchPreferenceDialog();
+			preferences.open();
+			RuntimePreferencePage prefs = new RuntimePreferencePage();
+			preferences.select(prefs);
+			prefs.editRuntime(prefs.getServerRuntimes().get(0).getName());
+			ServerRuntimeWizard runtimeWizard = new ServerRuntimeWizard();
+			if (runtimeWizard.getExecutionEnvironment().contains("1.6")) {
+				List<String> list = runtimeWizard.getExecutionEnvironments();
+				for (String temp : list) {
+					if (temp.contains("1.7") || temp.contains("1.8")) {
+						runtimeWizard.selectExecutionEnvironment(temp);
+						break;
+					}
+				}
+			}
+			runtimeWizard.finish();
+			preferences.ok();
+		}
+		new ServersView().getServer(base.getName()).start();
 
 		ProjectFactory.createProject(PROJECT_NAME, PROJECT_ARCHETYPE);
 	}
@@ -85,15 +116,15 @@ public class DeploymentEAPTest extends DefaultTest {
 
 		FuseServerManipulator.addModule(serverRequirement.getConfig().getName(), PROJECT_NAME);
 		assertTrue(FuseServerManipulator.hasServerModule(serverRequirement.getConfig().getName(), PROJECT_NAME));
-		new WaitUntil(new ConsoleHasText("(CamelContext: spring-context) started"));
-		new WaitUntil(new ConsoleHasText("Deployed \"wildfly-spring.war\""));
+		new WaitUntil(new ConsoleHasText("(CamelContext: spring-context) started"), TimePeriod.LONG);
+		new WaitUntil(new ConsoleHasText("Deployed \"wildfly-spring.war\""), TimePeriod.LONG);
 		BrowserView browser = new BrowserView();
 		browser.open();
 		browser.openPageURL("http://localhost:8080/wildfly-spring");
 		assertTrue(browser.getText().contains("Hello null"));
 		FuseServerManipulator.removeAllModules(serverRequirement.getConfig().getName());
-		new WaitUntil(new ConsoleHasText("(CamelContext: spring-context) is shutdown"));
-		new WaitUntil(new ConsoleHasText("Undeployed \"wildfly-spring.war\""));
+		new WaitUntil(new ConsoleHasText("(CamelContext: spring-context) is shutdown"), TimePeriod.LONG);
+		new WaitUntil(new ConsoleHasText("Undeployed \"wildfly-spring.war\""), TimePeriod.LONG);
 		assertFalse(FuseServerManipulator.hasServerModule(serverRequirement.getConfig().getName(), PROJECT_NAME));
 		browser.open();
 		browser.openPageURL("http://localhost:8080/wildfly-spring");
