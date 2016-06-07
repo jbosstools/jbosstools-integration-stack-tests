d0a0a86cc47f383b4f0da13ab18f80c49869c173
Tomáš Sedmík <tsedmik@redhat.com>
2016-06-07 13:39:32 +0200

    Fuse: Adjust test suites for runtime verification

--- tests/org.jboss.tools.fuse.ui.bot.test/src/org/jboss/tools/fuse/ui/bot/test/suite/RuntimeVerificationTests_Fuse.java
+++ tests/org.jboss.tools.fuse.ui.bot.test/src/org/jboss/tools/fuse/ui/bot/test/suite/RuntimeVerificationTests_Fuse.java
@@ -1,15 +1,13 @@
 package org.jboss.tools.fuse.ui.bot.test.suite;
 
-import junit.framework.TestSuite;
-
 import org.jboss.reddeer.junit.runner.RedDeerSuite;
 import org.jboss.tools.fuse.ui.bot.test.DataTransformationDeploymentTest;
 import org.jboss.tools.fuse.ui.bot.test.DeploymentTest;
 import org.jboss.tools.fuse.ui.bot.test.JMXNavigatorServerTest;
-import org.jboss.tools.fuse.ui.bot.test.QuickStartsTest;
-import org.jboss.tools.fuse.ui.bot.test.ServerTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite.SuiteClasses;
+
+import junit.framework.TestSuite;
 
 /**
  * Runs verification tests for a JBoss Fuse runtime.
@@ -19,10 +17,8 @@ import org.junit.runners.Suite.SuiteClasses;
 @SuiteClasses({
 	DataTransformationDeploymentTest.class,
 	DeploymentTest.class,
-	ServerTest.class,
-	JMXNavigatorServerTest.class,
-	QuickStartsTest.class })
+	JMXNavigatorServerTest.class })
 @RunWith(RedDeerSuite.class)
-public class FuseRuntimeVerificationTests extends TestSuite {
+public class RuntimeVerificationTests_Fuse extends TestSuite {
 
 }
