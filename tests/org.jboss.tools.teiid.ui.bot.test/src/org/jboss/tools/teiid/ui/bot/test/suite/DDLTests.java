58c8627a5c9a176c331552fae68c5c3403693a5d
Matus Makovy <mmakovy@redhat.com>
2016-06-13 12:07:41 +0200

    JBTIS-746 Reworked test suites

---
+++ tests/org.jboss.tools.teiid.ui.bot.test/src/org/jboss/tools/teiid/ui/bot/test/suite/DDLTests.java
@@ -0,0 +1,15 @@
+package org.jboss.tools.teiid.ui.bot.test.suite;
+
+import org.jboss.reddeer.junit.runner.RedDeerSuite;
+import org.jboss.tools.teiid.ui.bot.test.DynamicVdbTest;
+import org.jboss.tools.teiid.ui.bot.test.ImportDDLtest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite.SuiteClasses;
+
+@SuiteClasses({
+	ImportDDLtest.class,
+	DynamicVdbTest.class})
+@RunWith(RedDeerSuite.class)
+public class DDLTests {
+
+}
--- tests/org.jboss.tools.teiid.ui.bot.test/src/org/jboss/tools/teiid/ui/bot/test/suite/NonServerTests.java
+++ tests/org.jboss.tools.teiid.ui.bot.test/src/org/jboss/tools/teiid/ui/bot/test/suite/NonServerTests.java
@@ -1,14 +1,22 @@
 package org.jboss.tools.teiid.ui.bot.test.suite;
 
 import org.jboss.reddeer.junit.runner.RedDeerSuite;
+import org.jboss.tools.teiid.ui.bot.test.BasicTest;
 import org.jboss.tools.teiid.ui.bot.test.CloneAndCopyTest;
+import org.jboss.tools.teiid.ui.bot.test.ImportWizardTest;
+import org.jboss.tools.teiid.ui.bot.test.ModelRefactoringTest;
+import org.jboss.tools.teiid.ui.bot.test.ModelWizardTest;
 import org.jboss.tools.teiid.ui.bot.test.TransformationToolsTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite.SuiteClasses;
 
 @SuiteClasses({
 	CloneAndCopyTest.class,
-	TransformationToolsTest.class
+	TransformationToolsTest.class,
+	ImportWizardTest.class,
+	BasicTest.class,
+	ModelWizardTest.class,
+	ModelRefactoringTest.class
 })
 @RunWith(RedDeerSuite.class)
 public class NonServerTests {
