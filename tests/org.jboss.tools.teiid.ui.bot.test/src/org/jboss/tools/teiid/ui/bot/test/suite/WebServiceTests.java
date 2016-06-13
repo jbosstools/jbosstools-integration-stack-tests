7dd941984acc87c9a970ca093d05d4e1f3685278
Matus Makovy <mmakovy@redhat.com>
2016-06-13 12:07:41 +0200

    JBTIS-746 Reworked test suites

---
+++ tests/org.jboss.tools.teiid.ui.bot.test/src/org/jboss/tools/teiid/ui/bot/test/suite/WebServiceTests.java
@@ -0,0 +1,19 @@
+package org.jboss.tools.teiid.ui.bot.test.suite;
+
+import org.jboss.reddeer.junit.runner.RedDeerSuite;
+import org.jboss.tools.teiid.ui.bot.test.ConsumeRestWs;
+import org.jboss.tools.teiid.ui.bot.test.ConsumeSoapWs;
+import org.jboss.tools.teiid.ui.bot.test.CreateRestProcedureTest;
+import org.jboss.tools.teiid.ui.bot.test.WARTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite.SuiteClasses;
+
+@SuiteClasses({
+	ConsumeRestWs.class,
+	ConsumeSoapWs.class,
+	CreateRestProcedureTest.class,
+	WARTest.class})
+@RunWith(RedDeerSuite.class)
+public class WebServiceTests {
+
+}
