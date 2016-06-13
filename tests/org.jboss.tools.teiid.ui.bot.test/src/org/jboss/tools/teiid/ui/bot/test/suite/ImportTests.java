3eaa040e75cb30bb4b9ca995e505d124cc004fce
Matus Makovy <mmakovy@redhat.com>
2016-06-13 12:07:41 +0200

    JBTIS-746 Reworked test suites

--- tests/org.jboss.tools.teiid.ui.bot.test/src/org/jboss/tools/teiid/ui/bot/test/suite/ImportTests.java
+++ tests/org.jboss.tools.teiid.ui.bot.test/src/org/jboss/tools/teiid/ui/bot/test/suite/ImportTests.java
@@ -1,13 +1,21 @@
 package org.jboss.tools.teiid.ui.bot.test.suite;
 
 import org.jboss.reddeer.junit.runner.RedDeerSuite;
+import org.jboss.tools.teiid.ui.bot.test.FlatFileTest;
+import org.jboss.tools.teiid.ui.bot.test.GeometryTypeTest;
 import org.jboss.tools.teiid.ui.bot.test.JDBCImportWizardTest;
+import org.jboss.tools.teiid.ui.bot.test.LdapImportTest;
 import org.jboss.tools.teiid.ui.bot.test.SalesforceImportTest;
 import org.jboss.tools.teiid.ui.bot.test.TeiidConnectionImportTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite.SuiteClasses;
 
-@SuiteClasses({ TeiidConnectionImportTest.class, JDBCImportWizardTest.class, SalesforceImportTest.class, })
+@SuiteClasses({ TeiidConnectionImportTest.class, 
+	JDBCImportWizardTest.class, 
+	SalesforceImportTest.class, 
+	LdapImportTest.class, 
+	GeometryTypeTest.class, 
+	FlatFileTest.class })
 @RunWith(RedDeerSuite.class)
 public class ImportTests {
 
---
+++ tests/org.jboss.tools.teiid.ui.bot.test/src/org/jboss/tools/teiid/ui/bot/test/suite/OtherTests.java
@@ -0,0 +1,27 @@
+package org.jboss.tools.teiid.ui.bot.test.suite;
+
+import org.jboss.reddeer.junit.runner.RedDeerSuite;
+import org.jboss.tools.teiid.ui.bot.test.GuidesTest;
+import org.jboss.tools.teiid.ui.bot.test.PreviewModelTest;
+import org.jboss.tools.teiid.ui.bot.test.ProcedurePreviewTest;
+import org.jboss.tools.teiid.ui.bot.test.RecursiveCommonTableExpression;
+import org.jboss.tools.teiid.ui.bot.test.ServerManipulationTest;
+import org.jboss.tools.teiid.ui.bot.test.UDFTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * Test suite for all teiid bot tests
+ * 
+ * @author apodhrad, tsedmik
+ */
+@SuiteClasses({
+	PreviewModelTest.class,
+	ProcedurePreviewTest.class,
+	GuidesTest.class,
+	RecursiveCommonTableExpression.class,
+	UDFTest.class,
+	ServerManipulationTest.class})
+@RunWith(RedDeerSuite.class)
+public class OtherTests {
+}
