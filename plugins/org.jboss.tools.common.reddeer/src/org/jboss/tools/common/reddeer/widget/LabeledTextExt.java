7baad26422ec115cf8e136052c9c924dc586c950
Andrej Podhradsky <apodhrad@redhat.com>
2016-07-21 14:33:33 +0200

    JBTIS-811: Add an option to ignore asterisks in properties view

--- plugins/org.jboss.tools.common.reddeer/src/org/jboss/tools/common/reddeer/widget/LabeledTextExt.java
+++ plugins/org.jboss.tools.common.reddeer/src/org/jboss/tools/common/reddeer/widget/LabeledTextExt.java
@@ -5,7 +5,11 @@ import org.jboss.reddeer.swt.impl.text.AbstractText;
 public class LabeledTextExt extends AbstractText {
 
 	public LabeledTextExt(String label) {
-		super(null, 0, new WithLabelMatcherExt(label));
+		this(label, true);
+	}
+	
+	public LabeledTextExt(String label, boolean ignoreAsterisk) {
+		super(null, 0, new WithLabelMatcherExt(label, ignoreAsterisk));
 	}
 
 }
