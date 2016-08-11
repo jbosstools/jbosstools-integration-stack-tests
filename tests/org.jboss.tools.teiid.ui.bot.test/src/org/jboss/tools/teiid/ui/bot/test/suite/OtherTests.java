1e4884bedc4b5c1be1cfdc5e713d1379b99fc919
Matus Makovy <mmakovy@redhat.com>
2016-08-11 10:47:03 +0200

    JBTIS-829 - Add ReuseVDB test to 'Other tests' test suite

--- tests/org.jboss.tools.teiid.ui.bot.test/src/org/jboss/tools/teiid/ui/bot/test/suite/OtherTests.java
+++ tests/org.jboss.tools.teiid.ui.bot.test/src/org/jboss/tools/teiid/ui/bot/test/suite/OtherTests.java
@@ -5,6 +5,7 @@ import org.jboss.tools.teiid.ui.bot.test.GuidesTest;
 import org.jboss.tools.teiid.ui.bot.test.PreviewModelTest;
 import org.jboss.tools.teiid.ui.bot.test.ProcedurePreviewTest;
 import org.jboss.tools.teiid.ui.bot.test.RecursiveCommonTableExpression;
+import org.jboss.tools.teiid.ui.bot.test.ReuseVDBTest;
 import org.jboss.tools.teiid.ui.bot.test.ServerManipulationTest;
 import org.jboss.tools.teiid.ui.bot.test.UDFTest;
 import org.junit.runner.RunWith;
@@ -21,7 +22,8 @@ import org.junit.runners.Suite.SuiteClasses;
 	GuidesTest.class,
 	RecursiveCommonTableExpression.class,
 	UDFTest.class,
-	ServerManipulationTest.class})
+	ServerManipulationTest.class,
+	ReuseVDBTest.class})
 @RunWith(RedDeerSuite.class)
 public class OtherTests {
 }
