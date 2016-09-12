80950a8af5fdf0c7158b2a78a577056d57bde419
Jozef Marko <jomarko@redhat.com>
2016-09-12 10:12:30 +0200

    JBTIS-876: manual task actor updates

---
+++ plugins/org.jboss.tools.bpmn2.reddeer/src/org/jboss/tools/bpmn2/reddeer/editor/dialog/jbpm/ManualTaskActorDialog.java
@@ -0,0 +1,19 @@
+package org.jboss.tools.bpmn2.reddeer.editor.dialog.jbpm;
+
+import org.jboss.reddeer.swt.impl.button.PushButton;
+import org.jboss.reddeer.swt.impl.shell.DefaultShell;
+import org.jboss.reddeer.swt.impl.text.LabeledText;
+
+/**
+ * Dialog serves for actor addition into Manual task
+ * @author jomarko
+ *
+ */
+public class ManualTaskActorDialog {
+
+	public void addActor(String actorName) {
+		new DefaultShell("Edit Source for Input Parameter \"Actor Id\"");
+		new LabeledText("Script").setText(actorName);
+		new PushButton("OK").click();
+	}
+}
