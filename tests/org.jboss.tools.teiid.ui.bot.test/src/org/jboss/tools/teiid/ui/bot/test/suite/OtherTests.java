2e1ed5a00cd0818bb1ebfd593f7dad1767492fba
Matus Makovy <mmakovy@redhat.com>
2016-10-13 12:13:24 +0200

    JBTIS-928 - Add 2 new tests to test suites

--- tests/org.jboss.tools.teiid.ui.bot.test/src/org/jboss/tools/teiid/ui/bot/test/suite/OtherTests.java
+++ tests/org.jboss.tools.teiid.ui.bot.test/src/org/jboss/tools/teiid/ui/bot/test/suite/OtherTests.java
@@ -9,6 +9,7 @@ import org.jboss.tools.teiid.ui.bot.test.RecursiveCommonTableExpression;
 import org.jboss.tools.teiid.ui.bot.test.ReuseVDBTest;
 import org.jboss.tools.teiid.ui.bot.test.ServerManipulationTest;
 import org.jboss.tools.teiid.ui.bot.test.UDFTest;
+import org.jboss.tools.teiid.ui.bot.test.VDBEditorTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite.SuiteClasses;
 
@@ -20,6 +21,7 @@ import org.junit.runners.Suite.SuiteClasses;
 	UDFTest.class,
 	ServerManipulationTest.class,
 	ReuseVDBTest.class,
-	DataRolesTest.class})
+	DataRolesTest.class,
+	VDBEditorTest.class})
 @RunWith(RedDeerSuite.class)
 public class OtherTests {}
