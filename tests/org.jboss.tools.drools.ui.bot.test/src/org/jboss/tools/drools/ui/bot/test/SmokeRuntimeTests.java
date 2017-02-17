6cf9ed751018b052051ea9ec57dbdf0c463abb94
Tomas David <tdavid@redhat.com>
2017-02-17 13:21:06 +0100

    JBTIS-1032: remove runtime tests from smoke test suite

---
+++ tests/org.jboss.tools.drools.ui.bot.test/src/org/jboss/tools/drools/ui/bot/test/SmokeRuntimeTests.java
@@ -0,0 +1,24 @@
+package org.jboss.tools.drools.ui.bot.test;
+
+import junit.framework.TestSuite;
+
+import org.jboss.reddeer.junit.runner.RedDeerSuite;
+import org.jboss.tools.drools.ui.bot.test.smoke.ConvertProjectTest;
+import org.jboss.tools.drools.ui.bot.test.smoke.DroolsProjectTest;
+import org.jboss.tools.drools.ui.bot.test.smoke.KieNavigatorTest;
+import org.jboss.tools.drools.ui.bot.test.smoke.PerspectiveTest;
+import org.jboss.tools.drools.ui.bot.test.smoke.NewResourcesTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite.SuiteClasses;
+
+@SuiteClasses({
+	PerspectiveTest.class,
+	KieNavigatorTest.class,
+	DroolsProjectTest.class,
+	NewResourcesTest.class,
+	ConvertProjectTest.class
+})
+@RunWith(RedDeerSuite.class)
+public class SmokeRuntimeTests extends TestSuite {
+
+}
