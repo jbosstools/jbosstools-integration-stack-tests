67597c22ef1a1ae9fa20009909918bf6c64c1f08
Andrej Podhradsky <apodhrad@redhat.com>
2017-05-04 16:19:53 +0200

    Add test suites for SY tests with / without a runtime
    Add test suites for SY tests with resp. without a runtime

---
+++ tests/org.jboss.tools.switchyard.ui.bot.test/src/org/jboss/tools/switchyard/ui/bot/test/suite/WithRuntimeTests.java
@@ -0,0 +1,23 @@
+package org.jboss.tools.switchyard.ui.bot.test.suite;
+
+import junit.framework.TestSuite;
+
+import org.jboss.reddeer.junit.runner.RedDeerSuite;
+import org.jboss.tools.switchyard.ui.bot.test.ProjectExplorerProjectCreationTest;
+import org.jboss.tools.switchyard.ui.bot.test.ServerManagingDeploymentTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * Test Suite
+ * 
+ * @author apodhrad
+ *
+ */
+@SuiteClasses({
+	ProjectExplorerProjectCreationTest.class,
+	ServerManagingDeploymentTest.class })
+@RunWith(RedDeerSuite.class)
+public class WithRuntimeTests extends TestSuite {
+
+}
