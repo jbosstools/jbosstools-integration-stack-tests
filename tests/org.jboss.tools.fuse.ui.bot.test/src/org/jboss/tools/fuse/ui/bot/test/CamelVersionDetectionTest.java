03d4ae0265d07a3176c4c671e8e7fe6dfdcd6bcc
Dominik Jelinek <djelinek@redhat.com>
2017-05-29 10:33:37 +0200

    JBTIS-1017: Fuse - create a test which checks camel version detection for all available runtimes

---
+++ tests/org.jboss.tools.fuse.ui.bot.test/src/org/jboss/tools/fuse/ui/bot/test/CamelVersionDetectionTest.java
@@ -0,0 +1,74 @@
+package org.jboss.tools.fuse.ui.bot.test;
+
+import static org.jboss.reddeer.requirements.server.ServerReqState.PRESENT;
+import static org.jboss.tools.runtime.reddeer.requirement.ServerReqType.EAP;
+import static org.jboss.tools.runtime.reddeer.requirement.ServerReqType.Fuse;
+import static org.junit.Assert.assertTrue;
+
+import org.jboss.reddeer.core.handler.ShellHandler;
+import org.jboss.reddeer.junit.requirement.inject.InjectRequirement;
+import org.jboss.reddeer.junit.runner.RedDeerSuite;
+import org.jboss.tools.common.reddeer.view.ErrorLogView;
+import org.jboss.tools.fuse.reddeer.requirement.FuseRequirement;
+import org.jboss.tools.fuse.reddeer.requirement.FuseRequirement.Fuse;
+import org.jboss.tools.fuse.reddeer.wizard.NewFuseIntegrationProjectWizard;
+import org.jboss.tools.fuse.ui.bot.test.utils.ProjectFactory;
+import org.jboss.tools.runtime.reddeer.requirement.ServerRequirement.Server;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Tests wizard for creating a new Fuse Integration Project
+ * 
+ * @author djelinek
+ */
+@RunWith(RedDeerSuite.class)
+@Fuse(server = @Server(type = { Fuse, EAP }, state = PRESENT))
+public class CamelVersionDetectionTest {
+	
+	@InjectRequirement
+	private FuseRequirement serverRequirement;
+
+	/**
+	 * Prepare/Clean test environment
+	 */
+	@Before
+	@After
+	public void setupDeleteProjects() {
+		ProjectFactory.deleteAllProjects();
+		ErrorLogView log = new ErrorLogView();
+		log.open();
+		log.deleteLog();
+		ShellHandler.getInstance().closeAllNonWorbenchShells();
+	}
+	
+	/**
+	 * <p>
+	 * Tests 'Camel Version detection'
+	 * </p>
+	 * <b>Steps:</b>
+	 * <ol>
+	 * <li>Invoke <i>File --> New --> Fuse Integration Project</i> wizard</li>
+	 * <li>Set project name</li>
+	 * <li>Hit 'Next'</li>
+	 * <li>Select installed 'Target Runtime'</li>
+	 * <li>Check whether the detected version of Camel is same as expected runtime version of Camel</li>
+	 * </ol>
+	 */
+	@Test
+	public void testCamelVersionDetection() {
+		NewFuseIntegrationProjectWizard projectWizard = new NewFuseIntegrationProjectWizard();
+		projectWizard.open();
+		projectWizard.setProjectName("camel-version");
+		projectWizard.next();
+		projectWizard.selectTargetRuntime(projectWizard.getTargetRuntimes().get(1));
+		String detected = projectWizard.getCamelVersion();
+		String expected = serverRequirement.getConfig().getCamelVersion();
+		assertTrue("Camel detection failed -> Detected: '" + detected + "', Expected: '" + expected + "'",
+				detected.equals(expected));
+		projectWizard.cancel();
+	}
+
+}
