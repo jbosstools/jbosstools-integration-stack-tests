65cfddb3110438c2254a19d76f02bbb84a89cfba
mkralik3 <mkralik@redhat.com>
2018-03-12 12:56:28 +0100

    [TeiidDesigner] Add Reproducer - TEIIDDES-2888
    Signed-off-by: mkralik3 <mkralik@redhat.com>

--- tests/org.jboss.tools.teiid.ui.bot.test/src/org/jboss/tools/teiid/ui/bot/test/suite/ReproducersTest.java
+++ tests/org.jboss.tools.teiid.ui.bot.test/src/org/jboss/tools/teiid/ui/bot/test/suite/ReproducersTest.java
@@ -1,11 +1,14 @@
 package org.jboss.tools.teiid.ui.bot.test.suite;
 
 import org.eclipse.reddeer.junit.runner.RedDeerSuite;
+import org.jboss.tools.teiid.ui.bot.test.reproducers.TeiidDes2888Test;
 import org.jboss.tools.teiid.ui.bot.test.reproducers.TeiidDes3125Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite.SuiteClasses;
 
-@SuiteClasses({ TeiidDes3125Test.class })
+@SuiteClasses({ 
+    TeiidDes3125Test.class,
+    TeiidDes2888Test.class})
 @RunWith(RedDeerSuite.class)
 public class ReproducersTest {
 }
