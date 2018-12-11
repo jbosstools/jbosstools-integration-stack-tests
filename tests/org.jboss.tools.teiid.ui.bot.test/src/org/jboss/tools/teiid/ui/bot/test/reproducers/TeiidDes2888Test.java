88188d28589c63dfe59d188990fbb2c12c8b1afa
mkralik3 <mkralik@redhat.com>
2018-12-11 12:12:43 +0100

    Fix reproducer 2888 on Win
    Signed-off-by: mkralik3 <mkralik@redhat.com>

--- tests/org.jboss.tools.teiid.ui.bot.test/src/org/jboss/tools/teiid/ui/bot/test/reproducers/TeiidDes2888Test.java
+++ tests/org.jboss.tools.teiid.ui.bot.test/src/org/jboss/tools/teiid/ui/bot/test/reproducers/TeiidDes2888Test.java
@@ -3,10 +3,15 @@ package org.jboss.tools.teiid.ui.bot.test.reproducers;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 
+import org.eclipse.reddeer.common.platform.RunningPlatform;
+import org.eclipse.reddeer.common.wait.TimePeriod;
+import org.eclipse.reddeer.common.wait.WaitUntil;
 import org.eclipse.reddeer.junit.requirement.inject.InjectRequirement;
 import org.eclipse.reddeer.junit.runner.RedDeerSuite;
 import org.eclipse.reddeer.requirements.openperspective.OpenPerspectiveRequirement.OpenPerspective;
 import org.eclipse.reddeer.requirements.server.ServerRequirementState;
+import org.eclipse.reddeer.swt.condition.ShellIsActive;
+import org.eclipse.reddeer.swt.impl.button.OkButton;
 import org.jboss.tools.teiid.reddeer.connection.ConnectionProfileConstants;
 import org.jboss.tools.teiid.reddeer.connection.TeiidJDBCHelper;
 import org.jboss.tools.teiid.reddeer.dialog.GenerateDynamicVdbDialog;
@@ -66,6 +71,14 @@ public class TeiidDes2888Test {
         String contents = wizard.getContents();
         wizard.finish();
         assertThat(contents, containsString("DEFAULT '(''Stock'')'"));
+        
+        /*TEIIDDES-3222*/
+        if(RunningPlatform.isWindows()){
+            new WaitUntil(new ShellIsActive("Error"), TimePeriod.DEFAULT, false);
+            if(new ShellIsActive("Error").test()) {
+                new OkButton().click();
+            }
+        }
         
         modelExplorer.deployVdb(PROJECT_NAME, dynamicVdbName);
         TeiidJDBCHelper jdbchelper = new TeiidJDBCHelper(teiidServer, vdbName);
